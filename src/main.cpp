6c7c6504c03507fbe4d2d476997787527028cf7d
Daniel Cooke <dcooke@well.ox.ac.uk>
2015-02-04 09:23:54 +0000

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Octopus
+//
+//  Created by Daniel Cooke on 03/02/2015.
+//  Copyright (c) 2015 Oxford University. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
