44ac550d49ac7b4bd9885ad07ddfb52ad8ef84e8
Daniel Cooke <dcooke@well.ox.ac.uk>
2016-08-05 12:52:21 +0100

    Now appends path to test name and finds tests in 2 suites deep

--- build/cmake/modules/BoostTestHelpers.cmake
+++ build/cmake/modules/BoostTestHelpers.cmake
@@ -1,23 +1,34 @@
 # Generates a single test runner usable by CTest from a single test source file
 function(add_boost_test SOURCE_FILE_NAME DEPENDENCY_LIB)
-    get_filename_component(TEST_EXECUTABLE_NAME ${SOURCE_FILE_NAME} NAME_WE)
+    # get the relative path in the source tree
+    get_filename_component(test_path ${SOURCE_FILE_NAME} PATH)
     
+    # get the source name without extension
+    get_filename_component(test_name ${SOURCE_FILE_NAME} NAME_WE)
+    
+    # concatenate the relative path and name in an . separated identifier
+    string(REPLACE "/" "." test_concat "${test_path}/${test_name}")
+    
+    # strip the trailing "_tests" part from the test ID
+    string(REGEX REPLACE "_tests" "" test_id ${test_concat})
+    
+    # add a main for the tests
     add_definitions(-DBOOST_TEST_MAIN)
     
-    add_executable(${TEST_EXECUTABLE_NAME} ${SOURCE_FILE_NAME})
+    add_executable(${test_id} ${SOURCE_FILE_NAME})
     
-    target_link_libraries(${TEST_EXECUTABLE_NAME} ${DEPENDENCY_LIB}
-                          ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY})
+    target_link_libraries(${test_id} ${DEPENDENCY_LIB} ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY})
     
+    # collect all the tests in the source file
     file(READ "${SOURCE_FILE_NAME}" SOURCE_FILE_CONTENTS)
     string(REGEX MATCHALL "BOOST_AUTO_TEST_CASE\\( *([A-Za-z_0-9]+) *\\)"
            FOUND_TESTS ${SOURCE_FILE_CONTENTS})
     
     foreach(HIT ${FOUND_TESTS})
-        string(REGEX REPLACE ".*\\( *([A-Za-z_0-9]+) *\\).*" "\\1" TEST_NAME ${HIT})
+        string(REGEX REPLACE ".*\\( *([A-Za-z_0-9]+) *\\).*" "\\1" unit_test_name ${HIT})
         
-        add_test(NAME "${TEST_EXECUTABLE_NAME}.${TEST_NAME}"
-                 COMMAND ${TEST_EXECUTABLE_NAME}
-                 --run_test=${TEST_NAME} --catch_system_error=yes)
+        add_test(NAME "${test_id}.${unit_test_name}"
+                 COMMAND ${test_id}
+                 --run_test=/*/*/${unit_test_name} --catch_system_error=yes)
     endforeach()
 endfunction()
