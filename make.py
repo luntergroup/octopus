497d94980168bbcc7337aada5d64ea9015c68574
Daniel Cooke <dcooke@well.ox.ac.uk>
2016-08-05 10:53:41 +0100

    Optionally installs to /usr/local/bin

--- make.py
+++ make.py
@@ -3,6 +3,7 @@
 import os
 from subprocess import call
 import platform
+import argparse
 
 def is_unix():
     system = platform.system()
@@ -30,9 +31,21 @@ if not os.path.exists(bin_dir):
 
 os.chdir(octopus_build_dir) # so cmake doesn't pollute root directory
 
-call(["cmake", octopus_dir])
+parser = argparse.ArgumentParser()
+parser.add_argument('--root', help='Install into /usr/local/bin', action='store_true')
+args = vars(parser.parse_args())
 
-if is_unix():
-    call(["make", "install"])
+ret = 0
+if args["root"]:
+    ret = call(["cmake", "-DINSTALL_GLOBAL=ON", octopus_dir])
 else:
-    print("TODO: make for Windows!")
+    ret = call(["cmake", octopus_dir])
+
+if ret == 0:
+    if is_unix():
+        if args["root"]:
+            call(["sudo", "make", "install"])
+        else:
+            call(["make", "install"])
+    else:
+        print("TODO: make for Windows!")
