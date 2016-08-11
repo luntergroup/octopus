ee7dd73ecf9ab3c6800cf2347885a92daf6a2b7f
Daniel Cooke <dcooke@well.ox.ac.uk>
2016-08-11 14:55:30 +0100

    Better sampling
    Now checks the input regions actually contain some reads before trying
    to take samples from them.

--- src/utils/read_size_estimator.cpp
+++ src/utils/read_size_estimator.cpp
@@ -7,10 +7,14 @@
 #include <deque>
 #include <iterator>
 #include <algorithm>
+#include <utility>
+#include <cassert>
 
 #include "maths.hpp"
 
 namespace octopus {
+
+namespace {
 
 template <typename ForwardIt, typename RandomGenerator>
 ForwardIt random_select(ForwardIt first, ForwardIt last, RandomGenerator& g) {
@@ -25,30 +29,64 @@ ForwardIt random_select(ForwardIt first, ForwardIt last) {
     return random_select(first, last, gen);
 }
 
-auto estimate_read_size(const AlignedRead& read)
+auto estimate_dynamic_size(const AlignedRead& read) noexcept
 {
-    return sizeof(AlignedRead)
-    // Now the dynamically allocated bits
-    + sequence_size(read) * sizeof(char)
+    return sequence_size(read) * sizeof(char)
     + sequence_size(read) * sizeof(AlignedRead::BaseQuality)
     + read.cigar().size() * sizeof(CigarOperation)
     + contig_name(read).size()
     + (read.has_other_segment() ? sizeof(AlignedRead::Segment) : 0);
 }
 
+auto estimate_read_size(const AlignedRead& read) noexcept
+{
+    return sizeof(AlignedRead) + estimate_dynamic_size(read);
+}
+
+auto get_valid_sample_regions(const std::vector<SampleName>& samples,
+                              const InputRegionMap& input_regions,
+                              ReadManager& read_manager)
+{
+    InputRegionMap result {};
+    result.reserve(input_regions.size());
+    
+    for (const auto& p : input_regions) {
+        InputRegionMap::mapped_type contig_regions {};
+        
+        std::copy_if(std::cbegin(p.second), std::cend(p.second),
+                     std::inserter(contig_regions, std::begin(contig_regions)),
+                     [&] (const auto& region) {
+                         return read_manager.has_reads(samples, region);
+                     });
+        
+        if (!contig_regions.empty()) {
+            result.emplace(p.first, std::move(contig_regions));
+        }
+    }
+    
+    return result;
+}
+
+} // namespace
+
 boost::optional<std::size_t> estimate_mean_read_size(const std::vector<SampleName>& samples,
                                                      const InputRegionMap& input_regions,
                                                      ReadManager& read_manager,
                                                      const unsigned max_sample_size)
 {
-    assert(!input_regions.empty());
+    if (input_regions.empty()) return boost::none;
+    
+    const auto sample_regions = get_valid_sample_regions(samples, input_regions, read_manager);
+    
+    if (sample_regions.empty()) return boost::none;
     
     const auto num_samples_per_sample = max_sample_size / samples.size();
     
-    std::deque<std::size_t> read_sizes {};
+    std::deque<std::size_t> read_size_samples {};
     
+    // take read samples from each sample seperatly to ensure we cover each
     for (const auto& sample : samples) {
-        const auto it  = random_select(std::cbegin(input_regions), std::cend(input_regions));
+        const auto it = random_select(std::cbegin(sample_regions), std::cend(sample_regions));
         
         assert(!it->second.empty());
         
@@ -62,15 +100,13 @@ boost::optional<std::size_t> estimate_mean_read_size(const std::vector<SampleNam
         
         const auto reads = read_manager.fetch_reads(sample, test_region);
         
-        std::transform(std::cbegin(reads), std::cend(reads), std::back_inserter(read_sizes),
+        std::transform(std::cbegin(reads), std::cend(reads), std::back_inserter(read_size_samples),
                        estimate_read_size);
     }
     
-    if (read_sizes.empty()) {
-        return boost::none;
-    }
+    if (read_size_samples.empty()) return boost::none;
     
-    return static_cast<std::size_t>(maths::mean(read_sizes) + maths::stdev(read_sizes));
+    return static_cast<std::size_t>(maths::mean(read_size_samples) + maths::stdev(read_size_samples));
 }
 
 std::size_t default_read_size_estimate() noexcept
