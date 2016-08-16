90ae60529a554e97e1de6ead6f6cf848790cde2f
Daniel Cooke <dcooke@well.ox.ac.uk>
2016-08-16 19:59:05 +0100

    Add unit tests for MappableFlatSet

--- test/unit/containers/mappable_flat_set_tests.cpp
+++ test/unit/containers/mappable_flat_set_tests.cpp
@@ -17,6 +17,57 @@ using octopus::MappableFlatSet;
 BOOST_AUTO_TEST_SUITE(containers)
 BOOST_AUTO_TEST_SUITE(mappable_flat_set)
 
+BOOST_AUTO_TEST_CASE(emplace_works)
+{
+    MappableFlatSet<ContigRegion> set {};
+    
+    set.emplace(0, 0);
+    BOOST_REQUIRE_EQUAL(set.size(), 1);
+    set.emplace(0, 0);
+    BOOST_CHECK_EQUAL(set.size(), 1);
+    set.emplace(0, 1);
+    BOOST_REQUIRE_EQUAL(set.size(), 2);
+    set.emplace(0, 1);
+    BOOST_CHECK_EQUAL(set.size(), 2);
+    
+    set.emplace(0, 3);
+    set.emplace(1, 1);
+    set.emplace(2, 4);
+    set.emplace(4, 5);
+    
+    BOOST_CHECK_EQUAL(set.size(), 6);
+    BOOST_CHECK(std::is_sorted(std::cbegin(set), std::cend(set)));
+}
+
+BOOST_AUTO_TEST_CASE(range_insert_acceptes_empty_ranges)
+{
+    MappableFlatSet<ContigRegion> set {};
+    
+    std::vector<ContigRegion> empty {};
+    
+    BOOST_CHECK_NO_THROW(set.insert(std::cbegin(empty), std::cend(empty)));
+    BOOST_CHECK(set.empty());
+    
+    set.emplace(0, 1);
+    
+    BOOST_CHECK_NO_THROW(set.insert(std::cbegin(empty), std::cend(empty)));
+    BOOST_CHECK_EQUAL(set.size(), 1);
+}
+
+BOOST_AUTO_TEST_CASE(range_insert_accepts_unsorted_ranges)
+{
+    const ContigRegion r1 {0, 1}, r2 {0, 2}, r3 {1, 1}, r4 {0, 0}, r5 {0, 4}, r6 {2, 2}, r7 {0, 3}, r8 {1, 2};
+    
+    std::vector<ContigRegion> unsorted {r1, r2, r3, r4, r5, r6, r7, r8};
+    
+    MappableFlatSet<ContigRegion> set {};
+    
+    set.insert(std::cbegin(unsorted), std::cend(unsorted));
+    
+    BOOST_CHECK_EQUAL(set.size(), unsorted.size());
+    BOOST_CHECK(std::is_sorted(std::cbegin(set), std::cend(set)));
+}
+
 BOOST_AUTO_TEST_CASE(insert_hint_works)
 {
     const ContigRegion r1 {0, 1}, r2 {0, 2}, r3 {1, 1}, r4 {0, 0}, r5 {0, 4},
@@ -74,6 +125,169 @@ BOOST_AUTO_TEST_CASE(insert_hint_works)
     
     BOOST_CHECK_EQUAL(set.size(), unique_regions.size() + 1);
     BOOST_CHECK_EQUAL(*it, r11);
+    BOOST_CHECK(std::is_sorted(std::cbegin(set), std::cend(set)));
+}
+
+BOOST_AUTO_TEST_CASE(erase_maybe_removes_a_single_element)
+{
+    MappableFlatSet<ContigRegion> set {};
+    
+    ContigRegion r1 {}, r2 {0, 0}, r3 {0, 1}, r4 {0, 2}, r5 {1, 1}, r6 {1, 5};
+    
+    BOOST_CHECK_NO_THROW(set.erase(r1));
+    
+    set.emplace(0, 0);
+    
+    set.erase(r1);
+    
+    BOOST_CHECK(set.empty());
+    
+    set.insert(r2);
+    set.erase(r2);
+    
+    BOOST_CHECK(set.empty());
+    
+    set.insert(r3);
+    set.erase(r3);
+    
+    BOOST_CHECK(set.empty());
+    
+    set.insert(r2);
+    set.insert(r3);
+    set.insert(r4);
+    set.insert(r5);
+    
+    set.erase(r4);
+    
+    BOOST_CHECK_EQUAL(set.size(), 3);
+    
+    set.insert(r4);
+    set.insert(r6);
+    
+    set.erase(r5);
+    BOOST_CHECK_EQUAL(set.size(), 4);
+    BOOST_CHECK(std::is_sorted(std::cbegin(set), std::cend(set)));
+    set.erase(r3);
+    BOOST_CHECK_EQUAL(set.size(), 3);
+    BOOST_CHECK(std::is_sorted(std::cbegin(set), std::cend(set)));
+    set.erase(r6);
+    BOOST_CHECK_EQUAL(set.size(), 2);
+    BOOST_CHECK(std::is_sorted(std::cbegin(set), std::cend(set)));
+    set.erase(r4);
+    BOOST_CHECK_EQUAL(set.size(), 1);
+    BOOST_CHECK(std::is_sorted(std::cbegin(set), std::cend(set)));
+    set.erase(r2);
+    BOOST_CHECK(set.empty());
+}
+    
+BOOST_AUTO_TEST_CASE(erase_all_works_for_empty_range)
+{
+    MappableFlatSet<ContigRegion> set {};
+    
+    set.emplace(0, 0);
+    set.emplace(0, 1);
+    set.emplace(1, 1);
+    
+    std::vector<ContigRegion> empty {};
+    
+    BOOST_REQUIRE_NO_THROW(set.erase_all(std::cbegin(empty), std::cend(empty)));
+    
+    BOOST_CHECK_EQUAL(set.size(), 3);
+}
+    
+BOOST_AUTO_TEST_CASE(erase_all_is_unchanged_if_there_are_no_matches)
+{
+    const ContigRegion r1 {0, 1}, r2 {0, 2}, r3 {1, 1}, r4 {0, 0}, r5 {0, 4},
+    r6 {2, 2}, r7 {1, 1}, r8 {0, 2}, r9 {0, 3}, r10 {1, 2};
+    
+    std::vector<ContigRegion> regions {r1, r2, r3, r4, r5, r6, r7, r8, r9, r10};
+    
+    std::sort(std::begin(regions), std::end(regions));
+    
+    MappableFlatSet<ContigRegion> set {};
+    
+    auto n = set.erase_all(std::cbegin(regions), std::cend(regions));
+    
+    BOOST_CHECK_EQUAL(n, 0);
+    BOOST_CHECK(set.empty());
+    
+    set.insert(std::cbegin(regions), std::cend(regions));
+    
+    std::vector<ContigRegion> others {ContigRegion {1, 3}, ContigRegion {3, 3}, ContigRegion {3, 4}};
+    
+    const auto s = set.size();
+    
+    n = set.erase_all(std::cbegin(others), std::cend(others));
+    
+    BOOST_CHECK_EQUAL(n, 0);
+    BOOST_CHECK_EQUAL(set.size(), s);
+}
+
+BOOST_AUTO_TEST_CASE(erase_all_only_erases_given_elements_and_maintains_order)
+{
+    std::vector<ContigRegion> tmp {};
+    
+    MappableFlatSet<ContigRegion> set {};
+    
+    set.emplace(0, 0);
+    
+    tmp.emplace_back(0, 0);
+    
+    set.erase_all(std::cbegin(tmp), std::cend(tmp));
+    
+    BOOST_CHECK(set.empty());
+    
+    set.emplace(0, 0);
+    set.emplace(0, 1);
+    set.emplace(1, 1);
+    set.emplace(0, 3);
+    
+    set.erase_all(std::cbegin(tmp), std::cend(tmp));
+    
+    BOOST_CHECK_EQUAL(set.size(), 3);
+    
+    tmp.emplace_back(0, 3);
+    tmp.emplace_back(1, 1);
+    tmp.emplace_back(1, 2);
+    
+    set.erase_all(std::cbegin(tmp), std::cend(tmp));
+    
+    BOOST_CHECK_EQUAL(set.size(), 1);
+    
+    // Now a tricky one
+    
+    set.clear();
+    tmp.clear();
+    
+    BOOST_REQUIRE(set.empty());
+    
+    set.emplace(81, 82);
+    set.emplace(136, 137);
+    set.emplace(138, 138);
+    set.emplace(163, 171);
+    set.emplace(164, 164);
+    set.emplace(164, 165);
+    set.emplace(165, 166);
+    set.emplace(166, 167);
+    set.emplace(167, 168);
+    set.emplace(168, 169);
+    set.emplace(169, 170);
+    set.emplace(170, 171);
+    set.emplace(170, 179);
+    set.emplace(171, 172);
+    set.emplace(173, 174);
+    
+    tmp.emplace_back(163, 171);
+    tmp.emplace_back(164, 164);
+    tmp.emplace_back(164, 165);
+    tmp.emplace_back(170, 171);
+    tmp.emplace_back(170, 179);
+    
+    auto s = set.size();
+    
+    set.erase_all(std::cbegin(tmp), std::cend(tmp));
+    
+    BOOST_CHECK_EQUAL(set.size(), s - tmp.size());
     BOOST_CHECK(std::is_sorted(std::cbegin(set), std::cend(set)));
 }
 
