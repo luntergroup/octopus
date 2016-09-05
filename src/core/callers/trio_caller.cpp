2a369d37b90e9f266113c2be2cba1ad73906a52b
Daniel Cooke <dcooke@well.ox.ac.uk>
2016-09-05 15:55:47 +0100

    Write TrioCaller skeleton
    Write the method stubs

--- src/core/callers/trio_caller.cpp
+++ src/core/callers/trio_caller.cpp
@@ -3,8 +3,94 @@
 
 #include "trio_caller.hpp"
 
+#include <typeinfo>
+
+#include "basics/genomic_region.hpp"
+#include "core/types/allele.hpp"
+#include "core/types/variant.hpp"
+#include "core/types/haplotype.hpp"
+#include "core/types/genotype.hpp"
+#include "core/models/genotype/trio_model.hpp"
+#include "utils/germline_variant_call.hpp"
+#include "utils/reference_call.hpp"
+
 namespace octopus {
 
+TrioCaller::TrioCaller(Caller::Components&& components,
+                       Caller::Parameters general_parameters,
+                       Parameters specific_parameters)
+: Caller {std::move(components), std::move(general_parameters)}
+, parameters_ {std::move(specific_parameters)}
+{
+    if (parameters_.ploidy == 0) {
+        throw std::logic_error {"IndividualCaller: ploidy must be > 0"};
+    }
+}
 
+Caller::CallTypeSet TrioCaller::do_get_call_types() const
+{
+    return {std::type_index(typeid(GermlineVariantCall))};
+}
+
+std::unique_ptr<Caller::Latents>
+TrioCaller::infer_latents(const std::vector<Haplotype>& haplotypes,
+                          const HaplotypeLikelihoodCache& haplotype_likelihoods) const
+{
+    const CoalescentModel germline_prior_model {
+        Haplotype {mapped_region(haplotypes.front()), reference_},
+        parameters_.germline_prior_model_params
+    };
+    const DeNovoModel denovo_model {
+        parameters_.denovo_model_params
+    };
+    const model::TrioModel model {parameters_.trio, germline_prior_model, denovo_model};
+    
+    auto genotypes = generate_all_genotypes(haplotypes, parameters_.ploidy);
+    auto latents = model.evaluate(genotypes, genotypes, genotypes, haplotype_likelihoods);
+    
+    return nullptr;
+}
+
+boost::optional<double>
+TrioCaller::calculate_model_posterior(const std::vector<Haplotype>& haplotypes,
+                                      const HaplotypeLikelihoodCache& haplotype_likelihoods,
+                                      const Caller::Latents& latents) const
+{
+    return calculate_model_posterior(haplotypes, haplotype_likelihoods, dynamic_cast<const Latents&>(latents));
+}
+
+boost::optional<double>
+TrioCaller::calculate_model_posterior(const std::vector<Haplotype>& haplotypes,
+                                      const HaplotypeLikelihoodCache& haplotype_likelihoods,
+                                      const Latents& latents) const
+{
+    return boost::none;
+}
+
+std::vector<std::unique_ptr<VariantCall>>
+TrioCaller::call_variants(const std::vector<Variant>& candidates, const Caller::Latents& latents) const
+{
+    return call_variants(candidates, dynamic_cast<const Latents&>(latents));
+}
+
+std::vector<std::unique_ptr<VariantCall>>
+TrioCaller::call_variants(const std::vector<Variant>& candidates, const Latents& latents) const
+{
+    return {};
+}
+
+std::vector<std::unique_ptr<ReferenceCall>>
+TrioCaller::call_reference(const std::vector<Allele>& alleles, const Caller::Latents& latents,
+                           const ReadMap& reads) const
+{
+    return call_reference(alleles, dynamic_cast<const Latents&>(latents), reads);
+}
+
+std::vector<std::unique_ptr<ReferenceCall>>
+TrioCaller::call_reference(const std::vector<Allele>& alleles, const Latents& latents,
+                            const ReadMap& reads) const
+{
+    return {};
+}
     
 } // namespace octopus
--- src/core/callers/trio_caller.hpp
+++ src/core/callers/trio_caller.hpp
@@ -8,6 +8,9 @@
 #include <string>
 
 #include "caller.hpp"
+#include "core/types/trio.hpp"
+#include "core/models/mutation/coalescent_model.hpp"
+#include "core/models/mutation/denovo_model.hpp"
 
 namespace octopus {
 
@@ -16,22 +19,73 @@ class Variant;
 class VcfRecord;
 class ReadPipe;
 
-class TrioCaller// : public Caller
+class TrioCaller : public Caller
 {
 public:
+    struct Parameters
+    {
+        Trio trio;
+        Phred<double> min_variant_posterior;
+        Phred<double> min_refcall_posterior;
+        unsigned ploidy;
+        CoalescentModel::Parameters germline_prior_model_params;
+        DeNovoModel::Parameters denovo_model_params;
+    };
+    
     TrioCaller() = delete;
     
-    ~TrioCaller() = default;
+    TrioCaller(Caller::Components&& components,
+               Caller::Parameters general_parameters,
+               Parameters specific_parameters);
     
     TrioCaller(const TrioCaller&)            = delete;
     TrioCaller& operator=(const TrioCaller&) = delete;
     TrioCaller(TrioCaller&&)                 = delete;
     TrioCaller& operator=(TrioCaller&&)      = delete;
-
+    
+    ~TrioCaller() = default;
+    
 private:
-    const unsigned ploidy_;
-    const SampleName mother_, father_;
-    const double min_variant_posterior_ = 0.95;
+    class Latents;
+    
+    Parameters parameters_;
+    
+    CallTypeSet do_get_call_types() const override;
+    
+    std::unique_ptr<Caller::Latents>
+    infer_latents(const std::vector<Haplotype>& haplotypes,
+                  const HaplotypeLikelihoodCache& haplotype_likelihoods) const override;
+    
+    boost::optional<double>
+    calculate_model_posterior(const std::vector<Haplotype>& haplotypes,
+                              const HaplotypeLikelihoodCache& haplotype_likelihoods,
+                              const Caller::Latents& latents) const override;
+    
+    boost::optional<double>
+    calculate_model_posterior(const std::vector<Haplotype>& haplotypes,
+                              const HaplotypeLikelihoodCache& haplotype_likelihoods,
+                              const Latents& latents) const;
+    
+    std::vector<std::unique_ptr<VariantCall>>
+    call_variants(const std::vector<Variant>& candidates, const Caller::Latents& latents) const override;
+    
+    std::vector<std::unique_ptr<VariantCall>>
+    call_variants(const std::vector<Variant>& candidates, const Latents& latents) const;
+    
+    std::vector<std::unique_ptr<ReferenceCall>>
+    call_reference(const std::vector<Allele>& alleles, const Caller::Latents& latents,
+                   const ReadMap& reads) const override;
+    
+    std::vector<std::unique_ptr<ReferenceCall>>
+    call_reference(const std::vector<Allele>& alleles, const Latents& latents,
+                   const ReadMap& reads) const;
+};
+
+class TrioCaller::Latents : public Caller::Latents
+{
+public:
+    std::shared_ptr<HaplotypeProbabilityMap> haplotype_posteriors() const noexcept override { return nullptr; }
+    std::shared_ptr<GenotypeProbabilityMap> genotype_posteriors() const noexcept override { return nullptr; }
 };
     
 } // namespace octopus
