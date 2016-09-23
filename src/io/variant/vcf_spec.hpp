3dcd016243629754c2ac2bff35ae8700fd4629b1
Daniel Cooke <dcooke@well.ox.ac.uk>
2016-09-23 10:40:20 +0100

    Add deleted base to vcf spec

--- src/io/variant/vcf_spec.hpp
+++ src/io/variant/vcf_spec.hpp
@@ -19,6 +19,8 @@ namespace octopus { namespace vcfspec {
     
     VCF_SPEC_CONSTANT missingValue {"."};
     
+    constexpr char deletedBase {'*'};
+    
     namespace header {
     
     VCF_SPEC_CONSTANT lineOpener {"#"};
