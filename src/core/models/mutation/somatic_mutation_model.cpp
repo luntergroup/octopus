cee9598dd69569671ed976be947fe33d391cbe55
Daniel Cooke <dcooke@well.ox.ac.uk>
2016-10-09 21:11:37 +0100

    Switch to log space in somatic prior calculations
    Doing these calculations in normal space can lead to arithmetic overflow and screw up downstream calculations.

--- src/core/models/mutation/somatic_mutation_model.cpp
+++ src/core/models/mutation/somatic_mutation_model.cpp
@@ -25,23 +25,46 @@ double SomaticMutationModel::evaluate(const CancerGenotype<Haplotype>& genotype)
     const auto& germline = genotype.germline_genotype();
     const auto& somatic = genotype.somatic_element();
     const auto germline_log_prior = germline_model_.get().evaluate(germline);
-    const auto somatic_probability_given_germline = probability_of_somatic(somatic, germline);
-    return germline_log_prior + std::log(somatic_probability_given_germline);
+    return germline_log_prior + ln_probability_of_somatic(somatic, germline);
 }
 
-// p(somatic | germline) = 1 / M sum k = 1 -> M p(somatic | germline_k) (M = germline ploidy)
-double SomaticMutationModel::probability_of_somatic(const Haplotype& somatic, const Genotype<Haplotype>& germline) const
+// p(somatic | germline) = 1 / M sum [k = 1 -> M] p(somatic | germline_k) (M = germline ploidy)
+double SomaticMutationModel::ln_probability_of_somatic(const Haplotype& somatic, const Genotype<Haplotype>& germline) const
 {
-    return std::accumulate(std::cbegin(germline), std::cend(germline), 0.0,
-                           [this, &somatic](const double curr, const Haplotype& germline) {
-                               return curr + probability_of_somatic(somatic, germline);
-                           }) / germline.ploidy();
+    switch (germline.ploidy()) {
+    case 1: return ln_probability_of_somatic(somatic, germline);
+    case 2:
+    {
+        const static double ln2 {std::log(2)};
+        const auto a = ln_probability_of_somatic(somatic, germline[0]);
+        const auto b = ln_probability_of_somatic(somatic, germline[1]);
+        return maths::log_sum_exp(a, b) - ln2;
+    }
+    case 3:
+    {
+        const static double ln3 {std::log(2)};
+        const auto a = ln_probability_of_somatic(somatic, germline[0]);
+        const auto b = ln_probability_of_somatic(somatic, germline[1]);
+        const auto c = ln_probability_of_somatic(somatic, germline[3]);
+        return maths::log_sum_exp(a, b, c) - ln3;
+    }
+    default:
+    {
+        std::vector<double> tmp(germline.ploidy());
+        std::transform(std::cbegin(germline), std::cend(germline), std::begin(tmp),
+                       [this, &somatic] (const Haplotype& haplotype) {
+                           return ln_probability_of_somatic(somatic, haplotype);
+                       });
+        return maths::log_sum_exp(tmp) - std::log(germline.ploidy());
+    }
+    }
 }
 
-double SomaticMutationModel::probability_of_somatic(const Haplotype& somatic, const Haplotype& germline) const
+double SomaticMutationModel::ln_probability_of_somatic(const Haplotype& somatic, const Haplotype& germline) const
 {
+    // TODO: implement a proper model for this (snv/indel).
     const auto variants = difference(somatic, germline);
-    return std::pow(params_.somatic_mutation_rate, variants.size());
+    return variants.size() * params_.somatic_mutation_rate;
 }
 
 } // namespace octopus
