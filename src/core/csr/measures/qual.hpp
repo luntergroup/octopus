444af39a22bc9070a29e888b8ced4921bba9f28c
Daniel Cooke <dcooke@well.ox.ac.uk>
2016-11-01 09:01:36 +0000

    Add qual measure

---
+++ src/core/csr/measures/qual.hpp
@@ -0,0 +1,29 @@
+// Copyright (c) 2016 Daniel Cooke
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+#ifndef qual_hpp
+#define qual_hpp
+
+#include <string>
+#include <memory>
+#include <utility>
+
+#include "measure.hpp"
+
+namespace octopus {
+
+class VcfRecord;
+
+namespace csr  {
+
+class Qual : public Measure
+{
+    virtual double operator()(const VcfRecord& call) const override;
+    virtual std::string name() const override;
+};
+
+} // namespace csr
+} // namespace octopus
+
+
+#endif
