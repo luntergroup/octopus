57b096a7bb4355cd80c4502cb5f736d56e10d619
Daniel Cooke <dcooke@well.ox.ac.uk>
2016-11-02 10:34:04 +0000

    Add facet class

---
+++ src/core/csr/facets/facet.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2016 Daniel Cooke
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+#include "facet.hpp"
+
+namespace octopus { namespace csr {
+
+
+    
+} // namespace csr
+} // namespace octopus
