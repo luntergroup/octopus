e2e19f2847ff31f50efea5547d95ff7b7657ed8b
Daniel Cooke <dcooke@well.ox.ac.uk>
2016-11-08 18:45:42 +0000

    Set default max-region-to-assemble to 500

--- src/config/option_parser.cpp
+++ src/config/option_parser.cpp
@@ -305,7 +305,7 @@ OptionMap parse_options(const int argc, const char** argv)
      "The gap size used to generate local assembly fallback kmers")
     
     ("max-region-to-assemble",
-     po::value<int>()->default_value(1000),
+     po::value<int>()->default_value(500),
      "The maximum region size that can be used for local assembly")
     
     ("max-assemble-region-overlap",
