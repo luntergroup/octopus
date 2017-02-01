0dd60a85f1370ceb4ff1f71a1bafea9b888c50e4
Daniel Cooke <dcooke@well.ox.ac.uk>
2017-02-01 20:50:20 +0000

    Allow address base haplotype caching in DeNovoModel

--- src/core/models/mutation/denovo_model.hpp
+++ src/core/models/mutation/denovo_model.hpp
@@ -7,6 +7,8 @@
 #include <cstddef>
 #include <unordered_map>
 
+#include <boost/optional.hpp>
+
 #include "core/types/haplotype.hpp"
 
 namespace octopus {
@@ -19,8 +21,13 @@ public:
         double mutation_rate;
     };
     
+    enum class CachingStrategy { none, value, address };
+    
     DeNovoModel() = delete;
-    DeNovoModel(Parameters parameters, std::size_t num_haplotypes_hint = 1000);
+    
+    DeNovoModel(Parameters parameters,
+                std::size_t num_haplotypes_hint = 1000,
+                CachingStrategy caching = CachingStrategy::value);
     
     DeNovoModel(const DeNovoModel&)            = default;
     DeNovoModel& operator=(const DeNovoModel&) = default;
@@ -35,7 +42,13 @@ public:
 private:
     Parameters parameters_;
     std::size_t num_haplotypes_hint_;
-    mutable std::unordered_map<Haplotype, std::unordered_map<Haplotype, double>> cache_;
+    CachingStrategy caching_;
+    mutable std::unordered_map<Haplotype, std::unordered_map<Haplotype, double>> value_cache_;
+    mutable std::unordered_map<const Haplotype*, std::unordered_map<const Haplotype*, double>> address_cache_;
+    
+    double evaluate_uncached(const Haplotype& target, const Haplotype& given) const;
+    double evaluate_basic_cache(const Haplotype& target, const Haplotype& given) const;
+    double evaluate_address_cache(const Haplotype& target, const Haplotype& given) const;
 };
 
 } // namespace octopus
