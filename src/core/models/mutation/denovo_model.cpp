8a22232c56321378e3e79f0312e4700f164546b4
Daniel Cooke <dcooke@well.ox.ac.uk>
2017-02-01 20:50:20 +0000

    Allow address base haplotype caching in DeNovoModel

--- src/core/models/mutation/denovo_model.cpp
+++ src/core/models/mutation/denovo_model.cpp
@@ -3,6 +3,7 @@
 
 #include "denovo_model.hpp"
 
+#include <memory>
 #include <iterator>
 #include <algorithm>
 #include <numeric>
@@ -14,13 +15,23 @@
 #include "core/types/variant.hpp"
 //#include "../pairhmm/pair_hmm.hpp"
 
+#include <iostream>
+
 namespace octopus {
 
-DeNovoModel::DeNovoModel(Parameters parameters, std::size_t num_haplotypes_hint)
+DeNovoModel::DeNovoModel(Parameters parameters, std::size_t num_haplotypes_hint, CachingStrategy caching)
 : parameters_ {parameters}
 , num_haplotypes_hint_ {num_haplotypes_hint}
-, cache_ {num_haplotypes_hint}
-{}
+, caching_ {caching}
+, value_cache_ {}
+, address_cache_ {}
+{
+    if (caching_ == CachingStrategy::address) {
+        address_cache_.reserve(num_haplotypes_hint_);
+    } else if (caching == CachingStrategy::value) {
+        value_cache_.reserve(num_haplotypes_hint_);
+    }
+}
 
 //auto make_hmm_model(const double denovo_mutation_rate) noexcept
 //{
@@ -50,35 +61,75 @@ DeNovoModel::DeNovoModel(Parameters parameters, std::size_t num_haplotypes_hint)
 
 double DeNovoModel::evaluate(const Haplotype& target, const Haplotype& given) const
 {
-    const auto target_iter = cache_.find(target);
-    if (target_iter != std::cend(cache_)) {
+    if (caching_ == CachingStrategy::address) {
+        return evaluate_address_cache(target, given);
+    } else if (caching_ == CachingStrategy::value) {
+        return evaluate_basic_cache(target, given);
+    } else {
+        return evaluate_uncached(target, given);
+    }
+}
+
+// private methods
+
+double DeNovoModel::evaluate_uncached(const Haplotype& target, const Haplotype& given) const
+{
+    // TODO: make indel errors context based
+    const auto variants = target.difference(given);
+    return std::accumulate(std::cbegin(variants), std::cend(variants), 0.0,
+                           [this] (const double curr, const Variant& v) {
+                               double penalty {std::log(parameters_.mutation_rate)};
+//                               if (is_insertion(v)) {
+//                                   penalty *= alt_sequence_size(v);
+//                               } else if (is_deletion(v)) {
+//                                   penalty *= region_size(v);
+//                               }
+                               return curr + penalty;
+                           });
+//    const auto model = make_hmm_model(parameters_.mutation_rate);
+//    const auto result = hmm::evaluate(target.sequence(), pad(given.sequence(), sequence_size(target)), model);
+}
+
+double DeNovoModel::evaluate_basic_cache(const Haplotype& target, const Haplotype& given) const
+{
+    const auto target_iter = value_cache_.find(target);
+    if (target_iter != std::cend(value_cache_)) {
         const auto given_iter = target_iter->second.find(given);
         if (given_iter != std::cend(target_iter->second)) {
             return given_iter->second;
         }
     }
-    // TODO: make indel errors context based
-    const auto variants = target.difference(given);
-    const auto result = std::accumulate(std::cbegin(variants), std::cend(variants), 0.0,
-                                        [this] (const double curr, const Variant& v) {
-                                            double penalty {std::log(parameters_.mutation_rate)};
-//                                            if (is_insertion(v)) {
-//                                                penalty *= alt_sequence_size(v);
-//                                            } else if (is_deletion(v)) {
-//                                                penalty *= region_size(v);
-//                                            }
-                                            return curr + penalty;
-                                        });
-//    const auto model = make_hmm_model(parameters_.mutation_rate);
-//    const auto result = hmm::evaluate(target.sequence(), pad(given.sequence(), sequence_size(target)), model);
-    if (target_iter != std::cend(cache_)) {
+    const auto result = evaluate_uncached(target, given);
+    if (target_iter != std::cend(value_cache_)) {
         target_iter->second.emplace(given, result);
     } else {
-        auto p = cache_.emplace(std::piecewise_construct,
-                                std::forward_as_tuple(target),
-                                std::forward_as_tuple(num_haplotypes_hint_));
+        auto p = value_cache_.emplace(std::piecewise_construct,
+                                      std::forward_as_tuple(target),
+                                      std::forward_as_tuple(num_haplotypes_hint_));
         assert(p.second);
         p.first->second.emplace(given, result);
+    }
+    return result;
+}
+
+double DeNovoModel::evaluate_address_cache(const Haplotype& target, const Haplotype& given) const
+{
+    const auto target_iter = address_cache_.find(std::addressof(target));
+    if (target_iter != std::cend(address_cache_)) {
+        const auto given_iter = target_iter->second.find(std::addressof(given));
+        if (given_iter != std::cend(target_iter->second)) {
+            return given_iter->second;
+        }
+    }
+    const auto result = evaluate_uncached(target, given);
+    if (target_iter != std::cend(address_cache_)) {
+        target_iter->second.emplace(std::addressof(given), result);
+    } else {
+        auto p = address_cache_.emplace(std::piecewise_construct,
+                                        std::forward_as_tuple(std::addressof(target)),
+                                        std::forward_as_tuple(num_haplotypes_hint_));
+        assert(p.second);
+        p.first->second.emplace(std::addressof(given), result);
     }
     return result;
 }
