0dc3ae3528bc7cfd9259c61fd8f232ff394ff589
Daniel Cooke <dcooke@well.ox.ac.uk>
2017-02-03 01:56:47 +0000

    Allow parents to borrow unused child combination spaces

--- src/core/models/genotype/trio_model.cpp
+++ src/core/models/genotype/trio_model.cpp
@@ -213,7 +213,17 @@ auto reduce(std::vector<ParentsProbabilityPair>& parental,
             std::vector<GenotypeRefProbabilityPair>& child,
             const TrioModel::Options& options)
 {
-    return reduce(parental, options) || reduce(child, options);
+    auto result = reduce(child, options);
+    const auto max_reduction_count = get_sample_reduction_count(options.max_combinations);
+    if (child.size() < max_reduction_count) {
+        const auto child_leftover = max_reduction_count - child.size();
+        return reduce(parental,
+                      get_sample_reduction_count(options.min_combinations),
+                      max_reduction_count + child_leftover,
+                      options.max_removal_posterior_mass) || result;
+    } else {
+        return reduce(parental, options) || result;
+    }
 }
 
 double probability_of_parents(const Genotype<Haplotype>& mother,
