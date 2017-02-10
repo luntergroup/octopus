e5bd8ad7b9371a2db75be4e592f9c88bfcb8e5da
Daniel Cooke <dcooke@well.ox.ac.uk>
2017-02-10 18:57:33 +0000

    Implement simple Pedigree interface

--- src/basics/pedigree.cpp
+++ src/basics/pedigree.cpp
@@ -3,18 +3,102 @@
 
 #include "pedigree.hpp"
 
+#include <algorithm>
+#include <iterator>
+
 namespace octopus {
 
 // public methods
 
-void Pedigree::clear()
+void Pedigree::add_relationship(Member parent, Member child)
 {
-    tree_.clear();
+    const auto parent_vertex = get_member_vertex(parent);
+    const auto child_vertex  = get_member_vertex(child);
+    boost::add_edge(parent_vertex, child_vertex, tree_);
 }
 
-std::size_t Pedigree::size() const
+boost::optional<const SampleName&> Pedigree::mother_of(const SampleName& child) const
 {
-    return boost::num_vertices(tree_);
+    return parent_of(child, Member::Sex::female);
+}
+
+boost::optional<const SampleName&> Pedigree::father_of(const SampleName& child) const
+{
+    return parent_of(child, Member::Sex::male);
+}
+
+std::vector<Pedigree::Member> Pedigree::children_of(const Member& parent) const
+{
+    const auto children = boost::adjacent_vertices(members_.at(parent.name), tree_);
+    std::vector<Member> result {};
+    result.reserve(std::distance(children.first, children.second));
+    std::transform(children.first, children.second, std::back_inserter(result),
+                   [this] (const Vertex& v) { return tree_[v]; });
+    return result;
+}
+
+bool Pedigree::is_member(const SampleName& member) const noexcept
+{
+    return members_.count(member) == 1;
+}
+
+bool Pedigree::is_empty() const noexcept
+{
+    return members_.empty();
+}
+
+std::size_t Pedigree::size() const noexcept
+{
+    return members_.size();
+}
+
+void Pedigree::clear() noexcept
+{
+    tree_.clear();
+    members_.clear();
+}
+
+// private methods
+
+Pedigree::Vertex Pedigree::add_new_member(const Member& member)
+{
+    const auto result = boost::add_vertex(member, tree_);
+    members_.insert(std::make_pair(member.name, result));
+    return result;
+}
+
+Pedigree::Vertex Pedigree::get_member_vertex(const Member& member)
+{
+    if (!is_member(member.name)) {
+        return add_new_member(member);
+    } else {
+        return members_.at(member.name);
+    }
+}
+
+boost::optional<const SampleName&> Pedigree::parent_of(const SampleName& child, Member::Sex parent_gender) const
+{
+    const auto parents = boost::inv_adjacent_vertices(members_.at(child), tree_);
+    const auto itr = std::find_if(parents.first, parents.second,
+                                  [&] (const Vertex& v) { return tree_[v].gender == parent_gender; });
+    if (itr == parents.second) {
+        return boost::none;
+    } else {
+        return tree_[*itr].name;
+    }
+}
+
+// non-member methods
+
+boost::optional<Trio> make_trio(const SampleName& child, const Pedigree& pedigree)
+{
+    const auto mother = pedigree.mother_of(child);
+    const auto father = pedigree.father_of(child);
+    if (mother && father) {
+        return Trio {{*mother}, {*father}, {child}};
+    } else {
+        return boost::none;
+    }
 }
     
 } // namespace octopus
--- src/basics/pedigree.hpp
+++ src/basics/pedigree.hpp
@@ -5,18 +5,26 @@
 #define pedigree_hpp
 
 #include <vector>
+#include <unordered_map>
 #include <cstddef>
 
 #include <boost/graph/adjacency_list.hpp>
+#include <boost/optional.hpp>
 
 #include "config/common.hpp"
+#import "trio.hpp"
 
 namespace octopus {
 
 class Pedigree
 {
 public:
-    using Member = octopus::SampleName;
+    struct Member
+    {
+        enum class Sex { male, female };
+        SampleName name;
+        Sex gender;
+    };
     
     Pedigree() = default;
     
@@ -28,29 +36,32 @@ public:
     ~Pedigree() = default;
     
     void add_relationship(Member parent, Member child);
-    void clear();
-    std::size_t size() const;
     
-    bool is_child(Member child, Member parent) const;
-    bool is_descendant(Member descendant, Member ancestor) const;
-    unsigned num_children(Member parent) const;
-    std::vector<Member> get_children(Member parent) const;
-    std::vector<Member> get_descendants(Member parent) const;
-
+    boost::optional<const SampleName&> mother_of(const SampleName& child) const;
+    boost::optional<const SampleName&> father_of(const SampleName& child) const;
+    std::vector<Member> children_of(const Member& parent) const;
+    
+    bool is_member(const SampleName& member) const noexcept;
+    
+    bool is_empty() const noexcept;
+    std::size_t size() const noexcept;
+    void clear() noexcept;
+    
 private:
-    using Tree = boost::adjacency_list<
-    boost::listS, boost::listS, boost::bidirectionalS, Member, boost::no_property
-    >;
-    
+    using Tree = boost::adjacency_list<boost::listS, boost::listS, boost::bidirectionalS, Member, boost::no_property>;
     using Vertex = typename boost::graph_traits<Tree>::vertex_descriptor;
     using Edge   = typename boost::graph_traits<Tree>::edge_descriptor;
     
     Tree tree_;
+    std::unordered_map<SampleName, Vertex> members_;
     
-    std::vector<Member> roots_;
-    std::vector<Member> leafs_;
+    Vertex add_new_member(const Member& member);
+    Vertex get_member_vertex(const Member& member);
+    boost::optional<const SampleName&> parent_of(const SampleName& child, Member::Sex parent_gender) const;
 };
-    
+
+boost::optional<Trio> make_trio(const SampleName& child, const Pedigree& pedigree);
+
 } // namespace octopus
 
 #endif
