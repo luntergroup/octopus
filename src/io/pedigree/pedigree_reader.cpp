2c56912cc06bec1e039bf562667827f8deef338c
Daniel Cooke <dcooke@well.ox.ac.uk>
2017-02-10 17:47:52 +0000

    Add pedigree reader stub

---
+++ src/io/pedigree/pedigree_reader.cpp
@@ -0,0 +1,4 @@
+// Copyright (c) 2016 Daniel Cooke
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+#include "pedigree_reader.hpp"
---
+++ src/io/pedigree/pedigree_reader.hpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2016 Daniel Cooke
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+#ifndef pedigree_reader_hpp
+#define pedigree_reader_hpp
+
+
+class pedigree_reader {
+    
+};
+
+
+#endif
