33ed54e98137d1a56d959ccc6d5514e7fab011d4
Daniel Cooke <dcooke@well.ox.ac.uk>
2017-02-17 19:14:51 +0000

    Remove haplotype generation and filtering timers

--- src/timers.cpp
+++ src/timers.cpp
@@ -19,13 +19,10 @@ TimerArray misc_timer {};
 void init_timers()
 {
     init_timer.start(); init_timer.stop();
-    haplotype_generation_timer.start(); haplotype_generation_timer.stop();
     haplotype_likelihood_timer.start(); haplotype_likelihood_timer.stop();
-    haplotype_fitler_timer.start(); haplotype_fitler_timer.stop();
     latent_timer.start(); latent_timer.stop();
     phasing_timer.start(); phasing_timer.stop();
     calling_timer.start(); calling_timer.stop();
-    
     for (auto& timer : misc_timer) {
         timer.start();
         timer.stop();
@@ -36,25 +33,14 @@ void print_all_timers()
 {
     std::cout << "init timer" << '\n';
     std::cout << init_timer.format() << std::endl;
-    
-    std::cout << "haplotype generation timer" << '\n';
-    std::cout << haplotype_generation_timer.format() << std::endl;
-    
     std::cout << "likelihood timer" << '\n';
     std::cout << haplotype_likelihood_timer.format() << std::endl;
-    
-    std::cout << "haplotype fitler timer" << '\n';
-    std::cout << haplotype_fitler_timer.format() << std::endl;
-    
     std::cout << "latent timer" << '\n';
     std::cout << latent_timer.format() << std::endl;
-    
     std::cout << "calling timer" << '\n';
     std::cout << calling_timer.format() << std::endl;
-    
     std::cout << "phasing timer" << '\n';
     std::cout << phasing_timer.format() << std::endl;
-    
     for (std::size_t i {0}; i < misc_timer.size(); ++i) {
         std::cout << "misc timer " << i << '\n';
         std::cout << misc_timer[i].format() << '\n';
