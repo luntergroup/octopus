dd355cd3261ab777a8ef1197f03447b1f4d2acf3
Daniel Cooke <dcooke@well.ox.ac.uk>
2017-04-02 16:09:12 +0100

    Add virtual priming to PopulationPriorModel

--- src/core/models/genotype/population_prior_model.hpp
+++ src/core/models/genotype/population_prior_model.hpp
@@ -16,6 +16,7 @@ class PopulationPriorModel
 {
 public:
     using GenotypeReference = std::reference_wrapper<const Genotype<Haplotype>>;
+    using GenotypeIndiceVectorReference = std::reference_wrapper<const std::vector<unsigned>>;
     
     PopulationPriorModel() = default;
     
@@ -26,12 +27,25 @@ public:
     
     virtual ~PopulationPriorModel() = default;
     
+    void prime(const std::vector<Haplotype>& haplotypes) { do_prime(haplotypes); }
+    void unprime() noexcept { do_unprime(); }
+    bool is_primed() const noexcept { return check_is_primed(); }
+    
     double evaluate(const std::vector<Genotype<Haplotype>>& genotypes) const { return do_evaluate(genotypes); }
     double evaluate(const std::vector<GenotypeReference>& genotypes) const { return do_evaluate(genotypes); }
-
+    double evaluate(const std::vector<std::vector<unsigned>>& indices) const { return do_evaluate(indices); }
+    double evaluate(const std::vector<GenotypeIndiceVectorReference>& indices) const { return do_evaluate(indices); }
+    
 private:
+    std::vector<Haplotype> haplotypes_;
+    
     virtual double do_evaluate(const std::vector<Genotype<Haplotype>>& genotypes) const = 0;
     virtual double do_evaluate(const std::vector<GenotypeReference>& genotypes) const = 0;
+    virtual double do_evaluate(const std::vector<std::vector<unsigned>>& indices) const = 0;
+    virtual double do_evaluate(const std::vector<GenotypeIndiceVectorReference>& indices) const = 0;
+    virtual void do_prime(const std::vector<Haplotype>& haplotypes) {};
+    virtual void do_unprime() noexcept {};
+    virtual bool check_is_primed() const noexcept = 0;
 };
 
 } // namespace octopus
