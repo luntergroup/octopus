152c3ff1529e505c19a0ca98c6e09b8f6562e0f5
Daniel Cooke <dcooke@well.ox.ac.uk>
2017-04-08 18:12:10 +0100

    Decrease ProgressMeter tick length if log events are taking a long time

--- src/logging/progress_meter.cpp
+++ src/logging/progress_meter.cpp
@@ -22,11 +22,11 @@ namespace octopus {
 using utils::TimeInterval;
 
 namespace {
-    template <typename T>
-    unsigned num_digits(const T x)
-    {
-        return static_cast<unsigned>(std::to_string(x).size());
-    }
+
+template <typename T>
+unsigned num_digits(const T x)
+{
+    return static_cast<unsigned>(std::to_string(x).size());
 }
 
 template <typename T>
@@ -40,14 +40,11 @@ unsigned max_str_length(const T& p)
 unsigned max_position_str_length(const InputRegionMap& input_regions)
 {
     assert(!input_regions.empty());
-    
     unsigned result {0};
-    
     for (const auto& p : input_regions) {
         const auto curr = max_str_length(p);
         if (result < curr) result = curr;
     }
-    
     return result;
 }
 
@@ -62,15 +59,18 @@ unsigned calculate_position_tab_length(const T& region)
     return std::max(18u, max_position_str_length(region));
 }
 
+} // namespace
+
 ProgressMeter::ProgressMeter(InputRegionMap regions)
 : regions_ {std::move(regions)}
 , completed_regions_ {}
 , num_bp_to_search_ {sum_region_sizes(regions_)}
 , num_bp_completed_ {0}
-, percent_unitl_log_ {percent_block_size_}
-, percent_at_last_log_ {0}
+, percent_until_tick_ {max_tick_size_}
+, percent_at_last_tick_ {0}
 , start_ {std::chrono::system_clock::now()}
-, last_log_ {start_}
+, last_tick_ {start_}
+, tick_durations_ {}
 , done_ {false}
 , position_tab_length_ {}
 , block_compute_times_ {}
@@ -82,29 +82,31 @@ ProgressMeter::ProgressMeter(InputRegionMap regions)
 }
 
 ProgressMeter::ProgressMeter(GenomicRegion region)
-: ProgressMeter {
-    InputRegionMap {std::make_pair(region.contig_name(), InputRegionMap::mapped_type {region})}
-  }
+: ProgressMeter {InputRegionMap {std::make_pair(region.contig_name(), InputRegionMap::mapped_type {region})}}
 {}
 
 ProgressMeter::ProgressMeter(ProgressMeter&& other)
 {
     using std::move;
     std::lock_guard<std::mutex> lock {other.mutex_};
-    regions_             = move(other.regions_);
-    completed_regions_   = move(other.completed_regions_);
-    num_bp_to_search_    = move(other.num_bp_to_search_);
-    num_bp_completed_    = move(other.num_bp_completed_);
-    percent_block_size_  = move(other.percent_block_size_);
-    percent_unitl_log_   = move(other.percent_unitl_log_);
-    percent_at_last_log_ = move(other.percent_at_last_log_);
-    start_               = move(other.start_);
-    last_log_            = move(other.last_log_);
-    done_                = move(other.done_);
-    position_tab_length_ = move(other.position_tab_length_);
-    block_compute_times_ = move(other.block_compute_times_);
-    log_                 = move(other.log_);
+    regions_              = move(other.regions_);
+    completed_regions_    = move(other.completed_regions_);
+    num_bp_to_search_     = move(other.num_bp_to_search_);
+    num_bp_completed_     = move(other.num_bp_completed_);
+    max_tick_size_        = move(other.max_tick_size_);
+    curr_tick_size_       = move(other.curr_tick_size_);
+    percent_until_tick_   = move(other.percent_until_tick_);
+    percent_at_last_tick_ = move(other.percent_at_last_tick_);
+    start_                = move(other.start_);
+    last_tick_            = move(other.last_tick_);
+    tick_durations_       = move(other.tick_durations_);
+    done_                 = move(other.done_);
+    position_tab_length_  = move(other.position_tab_length_);
+    block_compute_times_  = move(other.block_compute_times_);
+    log_                  = move(other.log_);
 }
+
+namespace {
 
 double percent_completed(const std::size_t num_bp_completed, const std::size_t num_bp_to_search)
 {
@@ -118,7 +120,7 @@ auto percent_completed_str(const std::size_t num_bp_completed, const std::size_t
 
 std::string to_string(const TimeInterval& duration)
 {
-    std::ostringstream ss {};
+    std::ostringstream ss{};
     ss << duration;
     return ss.str();
 }
@@ -126,7 +128,7 @@ std::string to_string(const TimeInterval& duration)
 template <typename ForwardIt>
 auto mean_duration(ForwardIt first, ForwardIt last)
 {
-    return maths::mean(first, last, [] (const auto& d) { return d.count(); });
+    return maths::mean(first, last, [](const auto& d) { return d.count(); });
 }
 
 template <typename Container>
@@ -138,7 +140,7 @@ auto mean_duration(const Container& durations)
 template <typename ForwardIt>
 auto stdev_duration(ForwardIt first, ForwardIt last)
 {
-    return maths::stdev(first, last, [] (const auto& d) { return d.count(); });
+    return maths::stdev(first, last, [](const auto& d) { return d.count(); });
 }
 
 template <typename Container>
@@ -148,33 +150,30 @@ auto stdev_duration(const Container& durations)
 }
 
 template <typename Container>
+bool all_equal(const Container& c)
+{
+    return std::adjacent_find(std::cbegin(c), std::cend(c), std::not_equal_to<void> {}) == std::cend(c);
+}
+
+template <typename Container>
 void remove_outliers(Container& durations)
 {
-    if (durations.size() < 2) {
-        return;
-    }
-    if (std::adjacent_find(std::cbegin(durations), std::cend(durations),
-        std::not_equal_to<void> {}) == std::cend(durations)) {
-        return;
-    }
-    
-    auto it = std::min_element(std::begin(durations), std::end(durations));
-    if (it == std::begin(durations)) {
-        it = std::remove(it, std::end(durations), *it);
+    if (durations.size() < 2 || all_equal(durations)) return;
+    auto min_duration_itr = std::min_element(std::begin(durations), std::end(durations));
+    if (min_duration_itr == std::begin(durations)) {
+        min_duration_itr = std::remove(min_duration_itr, std::end(durations), *min_duration_itr);
     } else {
-        it = std::end(durations);
+        min_duration_itr = std::end(durations);
     }
-    
-    const auto mean  = mean_duration(std::begin(durations), it);
-    const auto stdev = stdev_duration(std::begin(durations), it);
+    const auto mean = mean_duration(std::begin(durations), min_duration_itr);
+    const auto stdev = stdev_duration(std::begin(durations), min_duration_itr);
     const auto min = std::max(0.0, mean - (2 * stdev));
     const auto max = mean + (2 * stdev);
-    
-    it = std::remove_if(it, std::end(durations),
-                        [=] (const auto& duration) {
-                            return duration.count() < min || duration.count() > max;
-                        });
-    durations.erase(it, std::end(durations));
+    min_duration_itr = std::remove_if(min_duration_itr, std::end(durations),
+                                      [=](const auto& duration) {
+                                          return duration.count() < min || duration.count() > max;
+                                      });
+    durations.erase(min_duration_itr, std::end(durations));
 }
 
 template <typename Container>
@@ -184,9 +183,11 @@ auto estimate_ttc(const std::chrono::system_clock::time_point now,
 {
     const auto mean_block_duration = mean_duration(durations);
     const auto estimated_remaining_ticks = static_cast<std::size_t>(num_remaining_blocks * mean_block_duration);
-    const std::chrono::milliseconds estimated_remaining_duration {estimated_remaining_ticks};
+    const std::chrono::milliseconds estimated_remaining_duration{estimated_remaining_ticks};
     return TimeInterval {now, now + estimated_remaining_duration};
 }
+
+} // namespace
 
 ProgressMeter::~ProgressMeter()
 {
@@ -204,11 +205,12 @@ ProgressMeter::~ProgressMeter()
     }
 }
 
-void ProgressMeter::set_percent_block_size(const double size)
+void ProgressMeter::set_max_tick_size(double percent)
 {
     block_compute_times_.clear(); // TODO: can we use old block times to estimate new block times?
-    percent_block_size_ = size;
-    percent_unitl_log_  = std::min(size, percent_unitl_log_);
+    max_tick_size_ = percent;
+    percent_until_tick_  = std::min(percent, percent_until_tick_);
+    curr_tick_size_ = std::min(max_tick_size_, curr_tick_size_);
 }
 
 void ProgressMeter::start()
@@ -218,7 +220,7 @@ void ProgressMeter::start()
         write_header();
     }
     start_ = std::chrono::system_clock::now();
-    last_log_ = start_;
+    last_tick_ = start_;
 }
 
 void ProgressMeter::resume()
@@ -254,8 +256,8 @@ void ProgressMeter::log_completed(const GenomicRegion& region)
     const auto new_bp_processed = merge(region);
     const auto new_percent_done = percent_completed(new_bp_processed, num_bp_to_search_);
     num_bp_completed_ += new_bp_processed;
-    percent_unitl_log_ -= new_percent_done;
-    if (percent_unitl_log_ <= 0) output_log(region);
+    percent_until_tick_ -= new_percent_done;
+    if (percent_until_tick_ <= 0) output_log(region);
 }
 
 // private methods
@@ -263,27 +265,20 @@ void ProgressMeter::log_completed(const GenomicRegion& region)
 ProgressMeter::RegionSizeType ProgressMeter::merge(const GenomicRegion& region)
 {
     const auto& contig_region = region.contig_region();
-    
     if (completed_regions_.count(region.contig_name()) == 0) {
         completed_regions_.emplace(std::piecewise_construct,
                                    std::forward_as_tuple(region.contig_name()),
                                    std::forward_as_tuple(std::initializer_list<ContigRegion>({contig_region})));
         return size(contig_region);
     }
-    
     auto& completed_regions = completed_regions_.at(region.contig_name());
-    
     if (completed_regions.has_overlapped(contig_region)) {
         auto overlapped = completed_regions.overlap_range(contig_region);
-        
         assert(size(overlapped) >= 1);
-        
         if (size(overlapped) == 1) {
             RegionSizeType result {0};
             const auto& interactor = overlapped.front();
-            
             if (contains(interactor, contig_region)) return result;
-            
             auto new_region = contig_region;
             if (begins_before(new_region, interactor)) {
                 result += left_overhang_size(new_region, interactor);
@@ -299,7 +294,6 @@ ProgressMeter::RegionSizeType ProgressMeter::merge(const GenomicRegion& region)
             completed_regions.insert(std::move(new_region));
             return result;
         }
-        
         auto new_region = contig_region;
         auto result     = size(new_region);
         result -= overlap_size(overlapped.front(), contig_region);
@@ -317,9 +311,7 @@ ProgressMeter::RegionSizeType ProgressMeter::merge(const GenomicRegion& region)
         completed_regions.insert(std::move(new_region));
         return result;
     }
-    
     completed_regions.insert(contig_region);
-    
     return size(contig_region);
 }
 
@@ -330,11 +322,9 @@ void ProgressMeter::write_header()
     const auto num_position_tab_whitespaces = position_tab_length_ - 8;
     const std::string pos_tab_lhs_pad((position_tab_length_ - 8) / 2, ' ');
     auto pos_tab_rhs_pad = pos_tab_lhs_pad;
-    
     if (num_position_tab_whitespaces % 2) {
         pos_tab_rhs_pad += ' ';
     }
-    
     stream(log_) << pos_tab_bar << "------------------------------------------------------";
     stream(log_) << pos_tab_lhs_pad << "current " << pos_tab_rhs_pad
     << "|                   |     time      |     estimated   ";
@@ -349,22 +339,15 @@ void ProgressMeter::output_log(const GenomicRegion& region)
     const auto now = std::chrono::system_clock::now();
     const TimeInterval duration {start_, now};
     const auto time_taken = to_string(duration);
-    
     if (percent_done >= 100) return;
-    
-    const auto percent_since_last_log = percent_done - percent_at_last_log_;
-    const auto num_blocks_completed = static_cast<std::size_t>(std::floor(percent_since_last_log / percent_block_size_));
+    const auto percent_since_last_log = percent_done - percent_at_last_tick_;
+    const auto num_blocks_completed = static_cast<std::size_t>(std::floor(percent_since_last_log / curr_tick_size_));
     std::string ttc {"-"};
-    
     if (num_blocks_completed > 0) {
-        const auto duration_since_last_log = std::chrono::duration_cast<DurationUnits>(now - last_log_);
+        const auto duration_since_last_log = std::chrono::duration_cast<DurationUnits>(now - last_tick_);
         const DurationUnits duration_per_block {duration_since_last_log.count() / num_blocks_completed};
-        
-        std::fill_n(std::back_inserter(block_compute_times_), num_blocks_completed,
-                    duration_per_block);
-        
-        const auto num_remaining_blocks = static_cast<std::size_t>((100.0 - percent_done) / percent_block_size_);
-        
+        std::fill_n(std::back_inserter(block_compute_times_), num_blocks_completed, duration_per_block);
+        const auto num_remaining_blocks = static_cast<std::size_t>((100.0 - percent_done) / curr_tick_size_);
         remove_outliers(block_compute_times_);
         ttc = to_string(estimate_ttc(now, block_compute_times_, num_remaining_blocks));
         assert(!ttc.empty());
@@ -372,9 +355,7 @@ void ProgressMeter::output_log(const GenomicRegion& region)
             ttc = "-";
         }
     }
-    
     const auto percent_completed = percent_completed_str(num_bp_completed_, num_bp_to_search_);
-    
     stream(log_) << position_pad(region)
                  << region.contig_name() << ':' << region.end()
                  << completed_pad(percent_completed)
@@ -383,10 +364,11 @@ void ProgressMeter::output_log(const GenomicRegion& region)
                  << time_taken
                  << ttc_pad(ttc)
                  << ttc;
-    
-    last_log_            = now;
-    percent_unitl_log_   = percent_block_size_;
-    percent_at_last_log_ = percent_done;
+    tick_durations_.emplace_back(now - last_tick_);
+    last_tick_            = now;
+    percent_until_tick_   = curr_tick_size_;
+    percent_at_last_tick_ = percent_done;
+    update_tick_size();
 }
 
 std::string ProgressMeter::position_pad(const GenomicRegion& completed_region) const
@@ -417,6 +399,25 @@ std::string ProgressMeter::ttc_pad(const std::string& ttc) const
 {
     if (ttc.size() >= 16) return {};
     return std::string(16 - ttc.size(), ' ');
+}
+
+void ProgressMeter::update_tick_size()
+{
+    constexpr std::size_t max_ticks_to_use {10};
+    const auto n = std::min(tick_durations_.size(), max_ticks_to_use);
+    if (n > 1) {
+        using D = decltype(tick_durations_)::value_type;
+        const auto tot = std::accumulate(std::crbegin(tick_durations_), std::next(std::crbegin(tick_durations_), n), D {0});
+        using s = std::chrono::seconds;
+        const auto mean_tick_duration = std::chrono::duration_cast<s>(tot).count() / n;
+        if (mean_tick_duration > 300) {
+            curr_tick_size_ = std::max(curr_tick_size_ / 100, min_tick_size_);
+        } else if (mean_tick_duration > 60) {
+            curr_tick_size_ = std::max(curr_tick_size_ / 10, min_tick_size_);
+        } else {
+            curr_tick_size_ = max_tick_size_;
+        }
+    }
 }
 
 } // namespace octopus
