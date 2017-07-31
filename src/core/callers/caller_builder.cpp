62481d9376fe6c74d804c796e2532988a66e3fd1
Daniel Cooke <dcooke@well.ox.ac.uk>
2017-07-31 16:34:23 +0100

    Set somatic indel rate in CallerBuilder

--- src/core/callers/caller_builder.cpp
+++ src/core/callers/caller_builder.cpp
@@ -339,10 +339,11 @@ CallerBuilder::CallerFactoryMap CallerBuilder::generate_factory() const
                                                       params_.ploidies.of(samples.front(), *requested_contig_),
                                                       params_.normal_sample,
                                                       make_cancer_prior_model(params_.snp_heterozygosity, params_.indel_heterozygosity),
-                                                      {params_.somatic_mutation_rate},
+                                                      {params_.somatic_mutation_rate, params_.somatic_mutation_rate},
                                                       params_.min_expected_somatic_frequency,
                                                       params_.credible_mass,
-                                                      params_.min_credible_somatic_frequency
+                                                      params_.min_credible_somatic_frequency,
+                                                      params_.max_joint_genotypes
                                                   });
         }},
         {"trio", [this, general_parameters = std::move(general_parameters)] () {
