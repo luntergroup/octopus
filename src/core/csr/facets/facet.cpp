927ab5dee830d9c2859e39775765f2454da1db08
Daniel Cooke <dcooke@well.ox.ac.uk>
2017-10-18 16:35:55 +0100

    Implement Facet skeleton

--- src/core/csr/facets/facet.cpp
+++ src/core/csr/facets/facet.cpp
@@ -5,7 +5,15 @@
 
 namespace octopus { namespace csr {
 
+bool operator==(const Facet& lhs, const Facet& rhs) noexcept
+{
+    return lhs.name() == rhs.name();
+}
 
-    
+bool operator==(const FacetWrapper& lhs, const FacetWrapper& rhs) noexcept
+{
+    return *lhs.base() == *rhs.base();
+}
+
 } // namespace csr
 } // namespace octopus
--- src/core/csr/facets/facet.hpp
+++ src/core/csr/facets/facet.hpp
@@ -4,14 +4,89 @@
 #ifndef facet_hpp
 #define facet_hpp
 
+#include <unordered_map>
+#include <string>
+#include <functional>
+#include <memory>
+
+#include <boost/variant.hpp>
+
+#include "concepts/equitable.hpp"
+#include "config/common.hpp"
+#include "core/types/haplotype.hpp"
+#include "core/types/genotype.hpp"
+
 namespace octopus { namespace csr {
 
-class Facet
+class Facet : public Equitable<Facet>
 {
+public:
+    using GenotypeMap = std::unordered_map<SampleName, MappableFlatSet<Genotype<Haplotype>>>;
+    using ResultType = boost::variant<int>;
     
+    Facet() = default;
+    
+    Facet(const Facet&)            = default;
+    Facet& operator=(const Facet&) = default;
+    Facet(Facet&&)                 = default;
+    Facet& operator=(Facet&&)      = default;
+    
+    virtual ~Facet() = default;
+    
+    const std::string& name() const noexcept { return do_name(); }
+    ResultType evaluate(const GenotypeMap& genotypes) const { return do_evaluate(genotypes); }
+    
+private:
+    virtual const std::string& do_name() const noexcept = 0;
+    virtual ResultType do_evaluate(const GenotypeMap& genotypes) const = 0;
 };
+
+bool operator==(const Facet& lhs, const Facet& rhs) noexcept;
+
+class FacetWrapper : public Equitable<FacetWrapper>
+{
+public:
+    FacetWrapper() = delete;
+    
+    FacetWrapper(std::unique_ptr<Facet> facet) : facet_ {std::move(facet)} {}
+    
+    FacetWrapper(const FacetWrapper&)            = delete;
+    FacetWrapper& operator=(const FacetWrapper&) = delete;
+    FacetWrapper(FacetWrapper&&)                 = default;
+    FacetWrapper& operator=(FacetWrapper&&)      = default;
+    
+    ~FacetWrapper() = default;
+    
+    const Facet* base() const noexcept { return facet_.get(); }
+    const std::string& name() const noexcept { return facet_->name(); }
+
+private:
+    std::unique_ptr<Facet> facet_;
+};
+
+bool operator==(const FacetWrapper& lhs, const FacetWrapper& rhs) noexcept;
 
 } // namespace csr
 } // namespace octopus
 
-#endif //OCTOPUS_FACET_HPP
+namespace std {
+
+template <> struct hash<octopus::csr::Facet>
+{
+    size_t operator()(const octopus::csr::Facet& facet) const noexcept
+    {
+        return hash<std::string>{}(facet.name());
+    }
+};
+
+template <> struct hash<octopus::csr::FacetWrapper>
+{
+    size_t operator()(const octopus::csr::FacetWrapper& facet) const noexcept
+    {
+        return hash<octopus::csr::Facet>{}(*facet.base());
+    }
+};
+
+} // namespace std
+
+#endif
