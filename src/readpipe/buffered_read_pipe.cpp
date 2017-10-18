0f919b730530fefefc32a05c1b9d4a70ef6e3fec
Daniel Cooke <dcooke@well.ox.ac.uk>
2017-10-18 16:34:31 +0100

    Implement BufferedReadPipe

--- src/readpipe/buffered_read_pipe.cpp
+++ src/readpipe/buffered_read_pipe.cpp
@@ -5,6 +5,44 @@
 
 namespace octopus {
 
+BufferedReadPipe::BufferedReadPipe(ReadPipe& source, std::size_t max_buffer_size)
+: source_ {source}
+, max_buffer_size_ {max_buffer_size}
+, buffer_ {}
+, buffered_region_ {}
+{}
 
+ReadPipe& BufferedReadPipe::source() noexcept
+{
+    return source_.get();
+}
+
+void BufferedReadPipe::clear() noexcept
+{
+    buffer_.clear();
+    buffered_region_ = boost::none;
+}
+
+ReadMap BufferedReadPipe::fetch_reads(const GenomicRegion& region) const
+{
+    setup_buffer(region);
+    return copy_overlapped(buffer_, region);
+}
+
+// private methods
+
+bool BufferedReadPipe::requires_new_fetch(const GenomicRegion& region) const noexcept
+{
+    return !buffered_region_ || !contains(*buffered_region_, region);
+}
+
+void BufferedReadPipe::setup_buffer(const GenomicRegion& region) const
+{
+    if (requires_new_fetch(region)) {
+        const auto max_region = expand_rhs(region, 2 * max_buffer_size_);
+        buffered_region_ = source_.get().read_manager().find_covered_subregion(max_region, max_buffer_size_);
+        buffer_ = source_.get().fetch_reads(*buffered_region_);
+    }
+}
 
 } // namespace octopus
--- src/readpipe/buffered_read_pipe.hpp
+++ src/readpipe/buffered_read_pipe.hpp
@@ -4,12 +4,22 @@
 #ifndef buffered_read_pipe_hpp
 #define buffered_read_pipe_hpp
 
+#include <functional>
+#include <cstddef>
+
+#include <boost/optional.hpp>
+
+#include "read_pipe.hpp"
+#include "basics/genomic_region.hpp"
+
 namespace octopus {
 
 class BufferedReadPipe
 {
 public:
     BufferedReadPipe() = delete;
+    
+    BufferedReadPipe(ReadPipe& source, std::size_t max_buffer_size);
     
     BufferedReadPipe(const BufferedReadPipe&)            = delete;
     BufferedReadPipe& operator=(const BufferedReadPipe&) = delete;
@@ -18,8 +28,20 @@ public:
     
     ~BufferedReadPipe() = default;
     
+    ReadPipe& source() noexcept;
+    
+    void clear() noexcept;
+    
+    ReadMap fetch_reads(const GenomicRegion& region) const;
+    
 private:
-
+    std::reference_wrapper<ReadPipe> source_;
+    std::size_t max_buffer_size_;
+    mutable ReadMap buffer_;
+    mutable boost::optional<GenomicRegion> buffered_region_;
+    
+    bool requires_new_fetch(const GenomicRegion& region) const noexcept ;
+    void setup_buffer(const GenomicRegion& region) const;
 };
 
 } // namespace octopus
