8b80bcc2e3228b7ca89caa16f7a1f9f70936d55c
Daniel Cooke <dcooke@well.ox.ac.uk>
2017-10-22 18:19:42 +0100

    Use octopus VCF spec constant

--- src/core/csr/measures/model_posterior.cpp
+++ src/core/csr/measures/model_posterior.cpp
@@ -4,13 +4,15 @@
 #include "model_posterior.hpp"
 
 #include "io/variant/vcf_record.hpp"
+#include "config/octopus_vcf.hpp"
 
 namespace octopus { namespace csr {
 
 Measure::ResultType ModelPosterior::do_evaluate(const VcfRecord& call, const FacetMap& facets) const
 {
-    if (call.has_info("MP")) {
-        return std::stod(call.info_value("MP").front());
+    namespace ovcf = octopus::vcf::spec;
+    if (call.has_info(ovcf::info::modelPosterior)) {
+        return std::stod(call.info_value(ovcf::info::modelPosterior)).front());
     } else {
         return boost::none;
     }
