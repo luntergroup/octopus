2c143c838bf31dcc6e4e139df9597b747fa4a1f1
Daniel Cooke <dcooke@well.ox.ac.uk>
2017-10-23 18:09:50 +0100

    Apply ModelPosterior Measure
    Use boost::static_visitor to resolve different Measure return types.

--- src/core/csr/filters/variant_call_filter_factory.cpp
+++ src/core/csr/filters/variant_call_filter_factory.cpp
@@ -9,31 +9,53 @@
 #include "core/csr/facets/facet_factory.hpp"
 #include "core/csr/measures/qual.hpp"
 #include "core/csr/measures/mean_mapping_quality.hpp"
+#include "core/csr/measures/model_posterior.hpp"
 #include "core/csr/measures/allele_frequency.hpp"
 #include "core/csr/measures/strand_bias.hpp"
+#include "core/csr/measures/mapping_quality_divergence.hpp"
 
 namespace octopus { namespace csr {
 
 struct LessThreshold : public ThresholdVariantCallFilter::Threshold
 {
-    explicit LessThreshold(Measure::ResultType target) : target_ {target} {}
+    explicit LessThreshold(double target) : visitor_ {target} {}
     bool operator()(Measure::ResultType value) const noexcept
     {
-        return value >= target_;
+        return boost::apply_visitor(visitor_, value);
     }
 private:
-    Measure::ResultType target_;
+    struct LessVisitor : public boost::static_visitor<bool>
+    {
+        explicit LessVisitor(double target) : target {target} {}
+        bool operator()(double value) const noexcept { return value >= target; }
+        bool operator()(boost::optional<double> value) const noexcept
+        {
+            return !value || *value >= target;
+        }
+        double target;
+    };
+    LessVisitor visitor_;
 };
 
 struct GreaterThreshold : public ThresholdVariantCallFilter::Threshold
 {
-    explicit GreaterThreshold(Measure::ResultType target) : target_ {target} {}
+    explicit GreaterThreshold(double target) : visitor_ {target} {}
     bool operator()(Measure::ResultType value) const noexcept
     {
-        return value <= target_;
+        return boost::apply_visitor(visitor_, value);
     }
 private:
-    Measure::ResultType target_;
+    struct GreaterVisitor : public boost::static_visitor<bool>
+    {
+        explicit GreaterVisitor(double target) : target {target} {}
+        bool operator()(double value) const noexcept { return value <= target; }
+        bool operator()(boost::optional<double> value) const noexcept
+        {
+            return !value || *value <= target;
+        }
+        double target;
+    };
+    GreaterVisitor visitor_;
 };
 
 std::unique_ptr<VariantCallFilter> VariantCallFilterFactory::make(const ReferenceGenome& reference,
@@ -44,15 +66,18 @@ std::unique_ptr<VariantCallFilter> VariantCallFilterFactory::make(const Referenc
     std::vector<MeasureWrapper> measures {};
     measures.push_back(make_wrapped_measure<Qual>());
     measures.push_back(make_wrapped_measure<MeanMappingQuality>());
-    measures.push_back(make_wrapped_measure<AlleleFrequency>());
-    measures.push_back(make_wrapped_measure<StrandBias>());
+    measures.push_back(make_wrapped_measure<ModelPosterior>());
+    //measures.push_back(make_wrapped_measure<AlleleFrequency>());
+    //measures.push_back(make_wrapped_measure<StrandBias>());
+    //measures.push_back(make_wrapped_measure<MappingQualityDivergence>());
     using TP = std::unique_ptr<ThresholdVariantCallFilter::Threshold>;
     std::vector<TP> thresholds {};
+    thresholds.push_back(std::make_unique<LessThreshold>(10.0));
+    thresholds.push_back(std::make_unique<LessThreshold>(30.0));
     thresholds.push_back(std::make_unique<LessThreshold>(20.0));
-    thresholds.push_back(std::make_unique<LessThreshold>(40.0));
-    thresholds.push_back(std::make_unique<LessThreshold>(0.2));
-    thresholds.push_back(std::make_unique<LessThreshold>(0.1));
-    return std::make_unique<ThresholdVariantCallFilter>(std::move(facet_factory), std::move(measures), std::move(thresholds));
+    //thresholds.push_back(std::make_unique<LessThreshold>(0.2));
+    //thresholds.push_back(std::make_unique<LessThreshold>(0.1));
+    //thresholds.push_back(std::make_unique<GreaterThreshold>(20.0));
     return std::make_unique<ThresholdVariantCallFilter>(std::move(facet_factory), std::move(measures),
                                                         std::move(thresholds), progress);
 }
