6503c2308e306b493b95b2274a8c96ae5340c5a2
Daniel Cooke <dcooke@well.ox.ac.uk>
2017-10-23 11:12:48 +0100

    Change allele_frequency header guard name

--- src/core/csr/measures/allele_frequency.hpp
+++ src/core/csr/measures/allele_frequency.hpp
@@ -1,8 +1,8 @@
 // Copyright (c) 2016 Daniel Cooke
 // Use of this source code is governed by the MIT license that can be found in the LICENSE file.
 
-#ifndef allele_frequency
-#define allele_frequency
+#ifndef allele_frequency_hpp
+#define allele_frequency_hpp
 
 #include <string>
 
