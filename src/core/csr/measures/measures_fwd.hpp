56cf6638d6ec0e6fb1af5021b8f580b1ff61b7e2
Daniel Cooke <dcooke@well.ox.ac.uk>
2017-10-26 11:58:13 +0100

    Add measures_fwd

---
+++ src/core/csr/measures/measures_fwd.hpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2016 Daniel Cooke
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+#ifndef measures_fwd_hpp
+#define measures_fwd_hpp
+
+#include "core/csr/measures/measure.hpp"
+#include "core/csr/measures/allele_frequency.hpp"
+#include "core/csr/measures/depth.hpp"
+#include "core/csr/measures/mapping_quality_divergence.hpp"
+#include "core/csr/measures/mean_mapping_quality.hpp"
+#include "core/csr/measures/model_posterior.hpp"
+#include "core/csr/measures/qual.hpp"
+#include "core/csr/measures/quality_by_depth.hpp"
+#include "core/csr/measures/strand_bias.hpp"
+
+#endif
