ba15e93c32d8fd228ab364d96f787a380aacd900
Daniel Cooke <dcooke@well.ox.ac.uk>
2017-10-27 14:04:44 +0100

    Improve MappingQualityDivergence
    Use maximum pairwise emperical KL divergence.

--- src/core/csr/measures/mapping_quality_divergence.cpp
+++ src/core/csr/measures/mapping_quality_divergence.cpp
@@ -3,15 +3,17 @@
 
 #include "mapping_quality_divergence.hpp"
 
+#include <algorithm>
+#include <iterator>
 #include <cmath>
+#include <limits>
+#include <numeric>
 
 #include <boost/variant.hpp>
 
 #include "io/variant/vcf_record.hpp"
 #include "basics/aligned_read.hpp"
 #include "../facets/read_assignments.hpp"
-
-#include "utils/read_stats.hpp"
 
 namespace octopus { namespace csr {
 
@@ -20,17 +22,96 @@ std::unique_ptr<Measure> MappingQualityDivergence::do_clone() const
     return std::make_unique<MappingQualityDivergence>(*this);
 }
 
+namespace {
+
+using MappingQualityVector = std::vector<AlignedRead::MappingQuality>;
+
+auto extract_mapping_qualities(const std::vector<AlignedRead>& reads)
+{
+    MappingQualityVector result(reads.size());
+    std::transform(std::cbegin(reads), std::cend(reads), std::begin(result),
+                   [](const auto& read) { return read.mapping_quality(); });
+    return result;
+}
+
+auto extract_mapping_qualities(const HaplotypeSupportMap& support)
+{
+    std::vector<MappingQualityVector> result{};
+    result.reserve(support.size());
+    for (const auto& p : support) {
+        result.push_back(extract_mapping_qualities(p.second));
+    }
+    return result;
+}
+
+using MappingQualityHistogram = std::vector<unsigned>;
+
+auto make_histogram(const MappingQualityVector& mapping_qualities, const unsigned prior = 1)
+{
+    using Q = AlignedRead::MappingQuality;
+    static constexpr unsigned max_qualities {std::numeric_limits<Q>::max() - std::numeric_limits<Q>::min()};
+    MappingQualityHistogram result(max_qualities, prior);
+    for (auto quality : mapping_qualities) {
+        ++result[quality];
+    }
+    return result;
+}
+
+auto convert_to_probabilities(const MappingQualityHistogram& counts)
+{
+    const auto norm = std::accumulate(std::cbegin(counts), std::cend(counts), 0.0);
+    std::vector<double> result(counts.size());
+    std::transform(std::cbegin(counts), std::cend(counts), std::begin(result),
+                   [norm] (const auto c) { return static_cast<double>(c) / norm; });
+    return result;
+}
+
+auto kl_divergence(const std::vector<double>& p, const std::vector<double>& q) noexcept
+{
+    return std::inner_product(std::cbegin(p), std::cend(p), std::cbegin(q), 0.0,
+                              std::plus<> {}, [] (const auto a, const auto b) {
+        return a * std::log(a / b);
+    });
+}
+
+auto symmetric_kl_divergence(const std::vector<double>& p, const std::vector<double>& q) noexcept
+{
+    return kl_divergence(p, q) + kl_divergence(q, p);
+}
+
+double calculate_max_pairwise_kl_divergence(const std::vector<std::vector<double>>& mq_distributions)
+{
+    double result {0};
+    const auto num_distributions = mq_distributions.size();
+    if (num_distributions < 2) return result;
+    for (std::size_t i {0}; i < num_distributions - 1; ++i) {
+        for (auto j = i + 1; j < num_distributions; ++j) {
+            result = std::max(result, symmetric_kl_divergence(mq_distributions[i], mq_distributions[j]));
+        }
+    }
+    return result;
+}
+
+double calculate_max_pairwise_kl_divergence(const std::vector<MappingQualityVector>& mapping_qualities)
+{
+    std::vector<std::vector<double>> mq_distributions {};
+    mq_distributions.reserve(mapping_qualities.size());
+    for (const auto& mqs : mapping_qualities) {
+        mq_distributions.push_back(convert_to_probabilities(make_histogram(mqs)));
+    }
+    return calculate_max_pairwise_kl_divergence(mq_distributions);
+}
+
+} // namespace
+
 Measure::ResultType MappingQualityDivergence::do_evaluate(const VcfRecord& call, const FacetMap& facets) const
 {
     const auto assignments = boost::get<ReadAssignments::ResultType>(facets.at("ReadAssignments").get());
     double max_divergence {0};
     for (const auto& p : assignments) {
         if (call.is_heterozygous(p.first)) {
-            if (p.second.size() == 2) {
-                auto h1_rmq = rmq_mapping_quality(p.second.cbegin()->second);
-                auto h2_rmq = rmq_mapping_quality(std::next(p.second.cbegin())->second);
-                max_divergence = std::max(max_divergence, std::abs(h1_rmq - h2_rmq));
-            }
+            auto mapping_qualities = extract_mapping_qualities(p.second);
+            max_divergence = std::max(max_divergence, calculate_max_pairwise_kl_divergence(mapping_qualities));
         }
     }
     return max_divergence;
