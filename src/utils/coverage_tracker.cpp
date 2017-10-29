31e01b8e786c410b69b98745d58afd1698982a12
Daniel Cooke <dcooke@well.ox.ac.uk>
2017-10-29 19:26:08 +0000

    Remove coverage_tracker.cpp
    CoverageTracker is a template so doesn't need .cpp

--- src/utils/coverage_tracker.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2016 Daniel Cooke
-// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
-
-#include "coverage_tracker.hpp"
-
-namespace octopus {
-
-} // namespace octopus
