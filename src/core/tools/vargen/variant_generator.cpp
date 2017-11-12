2658bdaa5e509e192542055d7b86b01983ac022b
Daniel Cooke <dcooke@well.ox.ac.uk>
2017-11-12 22:38:34 +0000

    Add assertion that variant generator results are sorted

--- src/core/tools/vargen/variant_generator.cpp
+++ src/core/tools/vargen/variant_generator.cpp
@@ -4,6 +4,7 @@
 #include "variant_generator.hpp"
 
 #include <algorithm>
+#include <cassert>
 
 #include "config/common.hpp"
 #include "basics/genomic_region.hpp"
@@ -64,23 +65,20 @@ namespace debug {
 std::vector<Variant> VariantGenerator::generate(const GenomicRegion& region)
 {
     std::vector<Variant> result {};
-    
     for (auto& generator : generators_) {
-        auto generator_result = generator->do_generate_variants(region); // results are sorted
-        
+        auto generator_result = generator->do_generate_variants(region);
+        assert(std::is_sorted(std::cbegin(generator_result), std::cend(generator_result)));
         if (debug_log_) {
             debug::print_generated_candidates(stream(*debug_log_), generator_result, generator->name());
         }
-        
-        auto it = result.insert(std::end(result),
-                                std::make_move_iterator(std::begin(generator_result)),
-                                std::make_move_iterator(std::end(generator_result)));
-        std::inplace_merge(std::begin(result), it, std::end(result));
+        auto itr = result.insert(std::end(result),
+                                 std::make_move_iterator(std::begin(generator_result)),
+                                 std::make_move_iterator(std::end(generator_result)));
+        std::inplace_merge(std::begin(result), itr, std::end(result));
     }
     // Each generator is guaranteed to return unique variants, but two generators can still
-    // propose the same varinat independently.
+    // propose the same variants independently.
     remove_duplicates(result);
-    
     return result;
 }
 
