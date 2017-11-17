0ddf46d5977844fc23056250c4483342c810cc8c
Daniel Cooke <dcooke@well.ox.ac.uk>
2017-11-17 09:53:24 +0000

    Add overload of generate_all_cancer_genotypes with index generation

--- src/core/types/cancer_genotype.cpp
+++ src/core/types/cancer_genotype.cpp
@@ -34,7 +34,7 @@ std::vector<CancerGenotype<Haplotype>>
 generate_all_cancer_genotypes(const std::vector<Genotype<Haplotype>>& germline_genotypes,
                               const std::vector<Haplotype>& somatic_haplotypes)
 {
-    auto haplotype_ptrs = make_all_shared(somatic_haplotypes);
+    const auto haplotype_ptrs = make_all_shared(somatic_haplotypes);
     std::vector<CancerGenotype<Haplotype>> result {};
     result.reserve(germline_genotypes.size() * somatic_haplotypes.size());
     for (const auto& genotype : germline_genotypes) {
@@ -44,6 +44,30 @@ generate_all_cancer_genotypes(const std::vector<Genotype<Haplotype>>& germline_g
             }
         }
     }
+    return result;
+}
+
+std::vector<CancerGenotype<Haplotype>>
+generate_all_cancer_genotypes(const std::vector<Genotype<Haplotype>>& germline_genotypes,
+                              const std::vector<std::vector<unsigned>>& germline_genotype_indices,
+                              const std::vector<Haplotype>& somatic_haplotypes,
+                              std::vector<std::pair<std::vector<unsigned>, unsigned>>& cancer_genotype_indices)
+{
+    assert(germline_genotypes.size() == germline_genotype_indices.size());
+    const auto haplotype_ptrs = make_all_shared(somatic_haplotypes);
+    std::vector<CancerGenotype<Haplotype>> result {};
+    const auto max_cancer_genotypes = germline_genotypes.size() * somatic_haplotypes.size();
+    result.reserve(max_cancer_genotypes);
+    cancer_genotype_indices.reserve(max_cancer_genotypes);
+    for (std::size_t g {0}; g < germline_genotypes.size(); ++g) {
+        for (unsigned h {0}; h < somatic_haplotypes.size(); ++h) {
+            if (!contains(germline_genotypes[g], *haplotype_ptrs[h])) {
+                result.emplace_back(germline_genotypes[g], haplotype_ptrs[h]);
+                cancer_genotype_indices.emplace_back(germline_genotype_indices[g], h);
+            }
+        }
+    }
+    cancer_genotype_indices.shrink_to_fit();
     return result;
 }
 
