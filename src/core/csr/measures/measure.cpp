0a9fa51e99cc193f7d84073b53780d3413e2159c
Daniel Cooke <dcooke@well.ox.ac.uk>
2018-01-16 18:02:22 +0000

    Specialise MeasureSerialiseVisitor for doubles

--- src/core/csr/measures/measure.cpp
+++ src/core/csr/measures/measure.cpp
@@ -3,6 +3,10 @@
 
 #include "measure.hpp"
 
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+
 #include <boost/lexical_cast.hpp>
 
 namespace octopus { namespace csr {
@@ -11,6 +15,12 @@ struct MeasureSerialiseVisitor : boost::static_visitor<>
 {
     std::string str;
     
+    void operator()(double value)
+    {
+        std::ostringstream ss;
+        ss << std::fixed << std::setprecision(3) << value;
+        str = ss.str();
+    }
     template <typename T>
     void operator()(const T& value)
     {
@@ -20,7 +30,7 @@ struct MeasureSerialiseVisitor : boost::static_visitor<>
     void operator()(const boost::optional<T>& value)
     {
         if (value) {
-            str = boost::lexical_cast<std::string>(*value);
+            (*this)(*value);
         } else {
             str = ".";
         }
