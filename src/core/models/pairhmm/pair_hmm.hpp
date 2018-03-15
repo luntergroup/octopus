7629fa8a3882675e02f0405503d25228a1108035
Daniel Cooke <dcooke@well.ox.ac.uk>
2018-03-15 10:40:47 +0000

    Add VariableGapExtendMutationModel and pair HMM evaluate overload

--- src/core/models/pairhmm/pair_hmm.hpp
+++ src/core/models/pairhmm/pair_hmm.hpp
@@ -29,6 +29,16 @@ struct MutationModel
     std::size_t lhs_flank_size = 0, rhs_flank_size = 0;
 };
 
+struct VariableGapExtendMutationModel
+{
+    using Penalty = std::int8_t;
+    using PenaltyVector = std::vector<Penalty>;
+    Penalty mutation;
+    const PenaltyVector& gap_open;
+    const std::vector<Penalty>& gap_extend;
+    short nuc_prior = 2;
+};
+
 struct VariableGapOpenMutationModel
 {
     using Penalty = std::int8_t;
@@ -36,12 +46,14 @@ struct VariableGapOpenMutationModel
     Penalty mutation;
     const PenaltyVector& gap_open;
     short gap_extend;
+    short nuc_prior = 2;
 };
 
 struct FlatGapMutationModel
 {
     std::int8_t mutation;
     short gap_open, gap_extend;
+    short nuc_prior = 2;
 };
 
 // p(target | truth, target_qualities, target_offset, model)
@@ -58,6 +70,13 @@ align(const std::string& target, const std::string& truth,
       const std::vector<std::uint8_t>& target_qualities,
       std::size_t target_offset,
       const MutationModel& model);
+
+// p(target | truth, model)
+//
+// Warning: The target must be contained by the truth by exactly
+// min_flank_pad() on either side.
+double evaluate(const std::string& target, const std::string& truth,
+                const VariableGapExtendMutationModel& model) noexcept;
 
 // p(target | truth, model)
 //
