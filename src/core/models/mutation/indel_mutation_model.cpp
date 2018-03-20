0dd5c1ac49206a78d7f758b27825299793930eac
Daniel Cooke <dcooke@well.ox.ac.uk>
2018-03-20 20:17:51 +0000

    Use max indel base probability across all repeats

--- src/core/models/mutation/indel_mutation_model.cpp
+++ src/core/models/mutation/indel_mutation_model.cpp
@@ -48,6 +48,18 @@ auto find_short_tandem_repeats(const Haplotype& haplotype)
     return find_exact_tandem_repeats(haplotype.sequence(), haplotype.mapped_region(), 1, max_repeat_period);
 }
 
+template <typename FordwardIt, typename Tp>
+auto fill_if_greater(FordwardIt first, FordwardIt last, const Tp& value)
+{
+    return std::transform(first, last, first, [&] (const auto& x) { return std::min(x, value); });
+}
+
+template <typename FordwardIt, typename Tp>
+auto fill_n_if_greater(FordwardIt first, std::size_t n, const Tp& value)
+{
+    return fill_if_greater(first, std::next(first, n), value);
+}
+
 } // namespace
 
 IndelMutationModel::ContextIndelModel IndelMutationModel::evaluate(const Haplotype& haplotype) const
@@ -55,19 +67,20 @@ IndelMutationModel::ContextIndelModel IndelMutationModel::evaluate(const Haploty
     const auto repeats = find_short_tandem_repeats(haplotype);
     ContextIndelModel result {};
     const auto haplotype_len = sequence_size(haplotype);
-    const auto& base_rates = indel_repeat_model_[0][0];
-    result.gap_open.resize(haplotype_len, base_rates.open);
-    result.gap_extend.resize(haplotype_len, base_rates.extend);
+    const auto& base_probabilities = indel_repeat_model_[0][0];
+    result.gap_open.resize(haplotype_len, base_probabilities.open);
+    result.gap_extend.resize(haplotype_len, base_probabilities.extend);
     for (const auto& repeat : repeats) {
+        assert(repeat.period > 0 && repeat.period <= params_.max_period);
         const auto repeat_offset = static_cast<std::size_t>(begin_distance(haplotype, repeat));
         const auto repeat_len = region_size(repeat);
         const auto num_repeats = static_cast<unsigned>(repeat_len / repeat.period);
         assert(num_repeats > 0);
         const auto& repeat_state = indel_repeat_model_[repeat.period][std::min(num_repeats, params_.max_periodicity)];
         assert(repeat_offset + repeat_len <= result.gap_open.size());
-        std::fill_n(std::next(std::begin(result.gap_open), repeat_offset), repeat_len, repeat_state.open);
+        fill_n_if_greater(std::next(std::begin(result.gap_open), repeat_offset), repeat_len, repeat_state.open);
         assert(repeat_offset + repeat_len <= result.gap_extend.size());
-        std::fill_n(std::next(std::begin(result.gap_extend), repeat_offset), repeat_len, repeat_state.extend);
+        fill_n_if_greater(std::next(std::begin(result.gap_extend), repeat_offset), repeat_len, repeat_state.extend);
     }
     return result;
 }
