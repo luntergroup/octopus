99961e696fffcaf1540f1840254c26a46dc87468
Daniel Cooke <dcooke@well.ox.ac.uk>
2018-06-21 21:25:40 +0100

    Add system_utils

---
+++ src/utils/system_utils.hpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2015-2018 Daniel Cooke
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+#ifndef system_utils_hpp
+#define system_utils_hpp
+
+#include <cstddef>
+
+namespace octopus {
+
+std::size_t get_max_open_files();
+
+} // namespace octopus
+
+#endif
