392fd1d0cb10c52ec63a09b318d0d2c1476b7bb9
Daniel Cooke <dcooke@well.ox.ac.uk>
2018-06-25 15:06:03 +0100

    Add SomaticRandomForestVariantCallFilter

---
+++ src/core/csr/filters/somatic_random_forest_filter.cpp
@@ -0,0 +1,42 @@
+// Copyright (c) 2015-2018 Daniel Cooke
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+#include "somatic_random_forest_filter.hpp"
+
+#include <utility>
+
+#include "../measures/is_somatic.hpp"
+#include "../measures/is_refcall.hpp"
+
+namespace octopus { namespace csr {
+
+SomaticRandomForestVariantCallFilter::SomaticRandomForestVariantCallFilter(FacetFactory facet_factory,
+                                                                           std::vector<MeasureWrapper> measures,
+                                                                           Path germline_forest, Path somatic_forest, Path refcall_forest,
+                                                                           OutputOptions output_config,
+                                                                           ConcurrencyPolicy threading,
+                                                                           Path temp_directory,
+                                                                           boost::optional<ProgressMeter&> progress)
+: ConditionalRandomForestFilter {
+    std::move(facet_factory),
+    std::move(measures),
+    {make_wrapped_measure<IsSomatic>(true), make_wrapped_measure<IsRefcall>(true)},
+    [] (const MeasureVector& measures) -> std::size_t {
+        assert(measures.size() == 2);
+        if (boost::get<bool>(measures.front())) {
+            return 1;
+        } else if (boost::get<bool>(measures.back())) {
+            return 2;
+        } else {
+            return 0;
+        }},
+    {std::move(germline_forest), std::move(somatic_forest), std::move(refcall_forest)},
+    std::move(output_config),
+    std::move(threading),
+    std::move(temp_directory),
+    progress
+}
+{}
+
+} // namespace csr
+} // namespace octopus
