53fd8a7726259a1aab65669027f612a6abe0b7f4
Daniel Cooke <dcooke@well.ox.ac.uk>
2019-01-27 20:56:39 +0000

    Implement basic error model generator

--- scripts/profiler.py
+++ scripts/profiler.py
@@ -1,57 +1,15 @@
 #!/usr/bin/env python3
 
 import argparse
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from math import log, log10, ceil
+from math import log10, ceil
 
-sns.set(style="darkgrid")
-
-base_palette = {'A': sns.color_palette()[2], 'C': sns.color_palette()[0], 'G': sns.color_palette()[5], 'T': sns.color_palette()[3],}
-
-def estimate_heterozygosity(polymorphisms, opportunities):
-    p = polymorphisms / max(2 * opportunities, 1)
-    return 2 * p * (1 - p)
-
-def aggregate_polymorphisms(indel_profile_df, aggregators=['platform', 'period', 'periods']):
-    if len(aggregators) == 0:
-        return indel_profile_df
-    polymorphism_attributes = ['platform', 'period', 'periods', 'tract_length', 'motif', 'reference_count', 'reference_span', 'indel_length', 'polymorphisms']
-    polymorphism_df = indel_profile_df[polymorphism_attributes]
-    aggregation_functions = {'platform': 'first', 'period': 'first', 'periods': 'first', 'reference_count': 'sum', 'reference_span': 'sum', 'polymorphisms': 'sum', 'tract_length': 'first'}
-    if 'indel_length' not in aggregators:
-        aggregation_functions['reference_count'] = 'first'
-        aggregation_functions['reference_span'] = 'first'
-        aggregation_functions['motif'] = 'first'
-        if 'motif' in aggregators:
-            polymorphism_df = polymorphism_df.groupby(aggregators).aggregate(aggregation_functions, axis='columns')
-        else:
-            polymorphism_df = polymorphism_df.groupby(aggregators + ['motif']).aggregate(aggregation_functions, axis='columns')
-        aggregation_functions['reference_count'] = 'sum'
-        aggregation_functions['reference_span'] = 'sum'
-    else:
-        aggregation_functions['indel_length'] = 'first'
-    if 'motif' in aggregators:
-        aggregation_functions['motif'] = 'first'
-    polymorphism_df = polymorphism_df.groupby(aggregators).aggregate(aggregation_functions, axis='columns')
-    polymorphism_df['polymorphism_rate'] = polymorphism_df.apply(lambda row: row.polymorphisms / max(row.reference_count, 1), axis=1)
-    polymorphism_attributes.append('polymorphism_rate')
-    polymorphism_df['heterozygosity'] = polymorphism_df.apply(lambda row: estimate_heterozygosity(row.polymorphisms, row.reference_count), axis=1)
-    polymorphism_attributes.append('heterozygosity')
-    if 'motif' not in aggregators:
-        del(polymorphism_attributes[polymorphism_attributes.index('motif')])
-    if 'indel_length' not in aggregators:
-        del(polymorphism_attributes[polymorphism_attributes.index('indel_length')])
-    return polymorphism_df[polymorphism_attributes]
-
-def aggregate_errors(df, aggregators=['platform', 'period', 'periods']):
+def aggregate_errors(df, aggregators=['period', 'periods']):
     if len(aggregators) == 0:
         return df
-    errors_attributes = ['platform', 'period', 'periods', 'tract_length', 'motif', 'indel_length', 'errors', 'reads']
+    errors_attributes = ['period', 'periods', 'tract_length', 'motif', 'indel_length', 'errors', 'reads']
     errors_df = df[errors_attributes]
-    aggregation_functions = {'platform': 'first', 'period': 'first', 'periods': 'first', 'errors': 'sum', 'reads': 'sum', 'tract_length': 'first'}
+    aggregation_functions = {'period': 'first', 'periods': 'first', 'errors': 'sum', 'reads': 'sum', 'tract_length': 'first'}
     if 'indel_length' not in aggregators:
         aggregation_functions['motif'] = 'first'
         aggregation_functions['reads'] = 'first'
@@ -73,82 +31,112 @@ def aggregate_errors(df, aggregators=['platform', 'period', 'periods']):
         del(errors_attributes[errors_attributes.index('indel_length')])
     return errors_df[errors_attributes]
 
-def read_indel_profile(csv, platform, model=None):
+def read_indel_profile(csv):
     result = pd.read_csv(csv)
-    result['platform'] = platform
-    result['polymorphism_rate'] = result.apply(lambda row: row.polymorphisms / max(row.reference_count, 1), axis=1)
-    result['heterozygosity'] = result.apply(lambda row: estimate_heterozygosity(row.polymorphisms, row.reference_count), axis=1)
     result['error_rate'] = result.apply(lambda row: row.errors / max(row.reads, 1), axis=1)
     result['tract_length'] = result.apply(lambda row: row.period * row.periods, axis=1)
-    if model is not None:
-        result['model'] = model
-    return result
-
-def read_indel_profiles(csvs, model=None):
-    dfs = []
-    for platform, csvs in csvs.items():
-        for csv in csvs:
-            dfs.append(read_indel_profile(csv, platform, model=model))
-    return pd.concat(dfs)
-
-def make_polymorphism_summaries(indel_profile_df):
-    result = {}
-    result['complex'] = indel_profile_df.query('motif == "N" and polymorphisms > 0')
-    result['homopolymer'] = indel_profile_df.query('period == 1 and periods > 1 and motif!="N"')
-    result['dinucleotide'] = indel_profile_df.query('period == 2 and periods > 1 and motif not in ["AA", "CC", "GG", "TT"]')
-    result['trinucleotide'] = indel_profile_df.query('period == 3 and periods > 0 and motif not in ["AAA", "CCC", "GGG", "TTT"]')
-    result['period'] = aggregate_polymorphisms(indel_profile_df)
-    result['length'] = aggregate_polymorphisms(indel_profile_df, ['platform', 'periods', 'period', 'indel_length'])
-    motif_aggregators = ['platform', 'periods', 'period', 'motif']
-    result['motif'] = aggregate_polymorphisms(indel_profile_df.query('motif != "N"'), motif_aggregators)
-    result['homopolymer-motif'] = aggregate_polymorphisms(result['homopolymer'], motif_aggregators)
-    result['dinucleotide-motif'] = aggregate_polymorphisms(result['dinucleotide'], motif_aggregators)
-    result['trinucleotide-motif'] = aggregate_polymorphisms(result['trinucleotide'], motif_aggregators)
     return result
 
 def make_error_summaries(indel_profile_df):
     result = {}
     result['complex'] = indel_profile_df.query('motif == "N" and errors > 0')
     result['homopolymer'] = indel_profile_df.query('period == 1 and periods > 0 and motif != "N"')
-    result['dinucleotide'] = indel_profile_df.query('period == 2 and periods > 0 and motif not in ["AA", "CC", "GG", "TT"]')
-    result['trinucleotide'] = indel_profile_df.query('period == 3 and periods > 0 and motif not in ["AAA", "CCC", "GGG", "TTT"]')
+    result['dinucleotide'] = indel_profile_df.query(
+        'period == 2 and periods > 0 and motif not in ["AA", "CC", "GG", "TT"]')
+    result['trinucleotide'] = indel_profile_df.query(
+        'period == 3 and periods > 0 and motif not in ["AAA", "CCC", "GGG", "TTT"]')
     result['period'] = aggregate_errors(indel_profile_df)
-    result['length'] = aggregate_errors(indel_profile_df, ['platform', 'periods', 'period', 'indel_length'])
-    motif_aggregators = ['platform', 'periods', 'period', 'motif']
+    result['length'] = aggregate_errors(indel_profile_df, ['periods', 'period', 'indel_length'])
+    motif_aggregators = ['periods', 'period', 'motif']
     result['homopolymer-motif'] = aggregate_errors(result['homopolymer'], motif_aggregators)
     result['dinucleotide-motif'] = aggregate_errors(result['dinucleotide'], motif_aggregators)
     result['trinucleotide-motif'] = aggregate_errors(result['trinucleotide'], motif_aggregators)
     return result
 
-def read_indel_profile_summaries(indel_profile_df):
-    result = {}
-    result['polymorphism'] = make_polymorphism_summaries(indel_profile_df)
-    result['error'] = make_error_summaries(indel_profile_df)
+def read_indel_error_profile_summaries(indel_profile_df):
+    return make_error_summaries(indel_profile_df)
+
+def read_indel_error_profile_and_summary(csv, model=None):
+    return read_indel_error_profile_summaries(read_indel_profile(csv))
+
+def rate_to_phred(rate):
+    return -10 * log10(rate + 1e-100)
+
+def complex_indel_penalty(profile_df):
+    return 43
+
+def get_repeat_error_df(profile_df, pattern, max_periods):
+    profile_index = 'period'
+    query_condition = "periods <= " + str(max_periods)
+    if type(pattern) == int:
+        query_condition += " and period == " + str(pattern)
+    else:
+        if len(pattern) == 1:
+            motif = pattern[0]
+            motif_len = len(motif)
+            query_condition += " and motif == '" + motif + "'"
+        else:
+            motif_len = len(pattern[0])
+            query_condition += " and (motif == '" + pattern[0] + "'"
+            for motif in pattern[1:]:
+                " or motif == '" + motif + "'"
+            query_condition += ")"
+        if motif_len == 1:
+            profile_index = 'homopolymer-motif'
+        elif motif_len == 2:
+            profile_index = 'dinucleotide-motif'
+        elif motif_len == 3:
+            profile_index = 'trinucleotide-motif'
+    result = profile_df[profile_index].query(query_condition)
+    result['phred_error'] = result.apply(lambda row: rate_to_phred(row['error_rate']), axis=1)
     return result
 
-def read_indel_profile_and_summary(csv, platform, model=None):
-    profile_df = read_indel_profile(csv, platform, model=model)
-    result = read_indel_profile_summaries(profile_df)
-    result['full'] = profile_df
-    return result
+def make_empirical_indel_error_model_helper(profile_df, pattern, max_periods=50):
+    repeat_error_df = get_repeat_error_df(profile_df, pattern, max_periods)
+    complex_penalty = complex_indel_penalty(profile_df)
+    penalties = [complex_penalty, complex_penalty]
+    for periods in range(2, max_periods):
+        try:
+            penalties.append(float(list(repeat_error_df.query('periods == ' + str(periods))['phred_error'])[0]))
+        except IndexError:
+            break
+    return [max(int(ceil(penalty)) + 2, 3) for penalty in penalties]
 
-def read_indel_profile_and_summaries(csvs, model=None):
-    profile_df = read_indel_profiles(csvs, model=model)
-    result = read_indel_profile_summaries(profile_df)
-    result['full'] = profile_df
+def max_lt(seq, val):
+    return max(v for v in seq if v < val)
+
+def smooth_empirical_model(open_model, extend_model=3):
+    # Rules: open[i] <= open[i + 1] + extend[i + 1]
+    head_penalties = open_model[:2]
+    max_penalty = max_lt(open_model[2:], 50)
+    result = [max_penalty, max_penalty]
+    for i, penalty in enumerate(open_model[2:], 2):
+        if i < len(open_model) - 1 and penalty < open_model[i + 1]:
+            penalty = int((result[i - 1] + min(result[i - 1], open_model[i + 1])) / 2)
+        result.append(max(min(penalty, result[i - 1]), result[i - 1] - extend_model))
+    result[:2] = head_penalties
     return result
 
 def make_octopus_indel_error_model(profile_df):
-    # Rules: open[i] <= open[i + 1] + extend[i + 1]
-    df = profiles_df['error']['period'].query('platform == "HiSeq-GIAB-HG005" and period == 3 and periods <= 50')
-    df['phred_error'] = df.apply(lambda row: -10 * log10(row['error_rate'] + 1e-100), axis=1)
-    penalties = [53, 53]
-    for periods in range(2, 45):
-        penalties.append(float(list(df.query('periods == ' + str(periods))['phred_error'])[0]))
-    print(','.join([str(p) for p in [max(int(ceil(penalty)) + 2, 3) for penalty in penalties]]))
+    result = {}
+    for pattern in [('A', 'T'), ('C', 'G'), 2, 3]:
+        model = smooth_empirical_model(make_empirical_indel_error_model_helper(profile_df, pattern))
+        if type(pattern) == int:
+            result[pattern * 'N'] = model
+        else:
+            for motif in pattern:
+                result[motif] = model
+    return result
+
+def write_error_model(error_model, out_path):
+    with open(out_path, 'w') as file:
+        for motif, model in error_model.items():
+            file.write(motif + ":" + ','.join([str(p) for p in model]) + '\n')
 
 def main(options):
-    profiles_df = read_indel_profile_and_summaries()
+    profiles_df = read_indel_error_profile_and_summary(options.profile)
+    error_model = make_octopus_indel_error_model(profiles_df)
+    write_error_model(error_model, options.output)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
