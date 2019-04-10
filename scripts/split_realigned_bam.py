96a2dfb76452ca416ec2a9242443f58bd59b78d1
Daniel Cooke <dcooke@well.ox.ac.uk>
2019-04-10 12:50:21 +0100

    Add "assigned_only" and "sort" options to 'split_realigned_bam.py'

--- scripts/split_realigned_bam.py
+++ scripts/split_realigned_bam.py
@@ -3,6 +3,8 @@
 import argparse
 import pysam as ps
 from pathlib import Path
+from os.path import isfile
+from os import rename
 
 def get_haplotype_id(read):
     try:
@@ -10,23 +12,39 @@ def get_haplotype_id(read):
     except KeyError:
         return None
 
+def bam_index_exists(bam_fname):
+    return isfile(bam_fname.with_suffix(bam_fname.suffix + '.bai'))
+
+def index_bam(bam_fname, sort_if_needed=False):
+    ps.index(str(bam_fname))
+    if not bam_index_exists(bam_fname):
+        sorted_bam_fname = str(bam_fname).replace('.bam', '.sorted.tmp.bam')
+        ps.sort('-o', sorted_bam_fname, str(bam_fname))
+        rename(sorted_bam_fname, bam_fname)
+        ps.index(str(bam_fname))
+
 def main(options):
+    if not isfile(options.bam):
+        print('Input realigned BAM ' + options.bam + ' does not exist')
+        exit(1)
     in_bam = ps.AlignmentFile(options.bam)
     out_bam_prefix = Path(options.output)
     variant_out_bams = [ps.AlignmentFile(str(out_bam_prefix) + '_0.bam', 'wb', template=in_bam)]
-    ref_out_bam = ps.AlignmentFile(str(out_bam_prefix) + '.bam', 'wb', template=in_bam)
+    ref_out_bam = None if options.assigned_only else ps.AlignmentFile(str(out_bam_prefix) + '.bam', 'wb', template=in_bam)
     for read in in_bam:
         haplotype_id = get_haplotype_id(read)
         if haplotype_id is None:
-            ref_out_bam.write(read)
+            if ref_out_bam is not None:
+                ref_out_bam.write(read)
         else:
             if len(variant_out_bams) < haplotype_id + 1:
                 for n in range(len(variant_out_bams), haplotype_id + 1):
                     variant_out_bams.append(ps.AlignmentFile(str(out_bam_prefix) + '_' + str(n) + '.bam', 'wb', template=in_bam))
             variant_out_bams[haplotype_id].write(read)
     for bam in [ref_out_bam] + variant_out_bams:
-        bam.close()
-        ps.index(bam.filename)
+        if bam is not None:
+            bam.close()
+            index_bam(Path(bam.filename.decode("utf-8")), sort_if_needed=options.sort)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -38,5 +56,13 @@ if __name__ == '__main__':
                         type=str,
                         required=True,
                         help='Output prefix')
+    parser.add_argument('-A', '--assigned_only',
+                        default=False,
+                        action='store_true',
+                        help='Do not output unassigned reads')
+    parser.add_argument('-S', '--sort',
+                        default=False,
+                        action='store_true',
+                        help='Sort output by position if required')
     parsed, unparsed = parser.parse_known_args()
     main(parsed)
