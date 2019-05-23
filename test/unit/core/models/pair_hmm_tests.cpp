9af9c5e638079a3f755aa3e29246506e9d64f2a7
Daniel Cooke <dcooke@well.ox.ac.uk>
2019-05-23 13:39:52 +0100

    Move Gerton's tests into unit test framework

--- test/unit/core/models/pair_hmm_tests.cpp
+++ test/unit/core/models/pair_hmm_tests.cpp
@@ -7,6 +7,8 @@
 #include <vector>
 #include <cstddef>
 #include <algorithm>
+#include <utility>
+#include <iostream>
 
 #include "core/models/pairhmm/simd_pair_hmm.hpp"
 
@@ -17,54 +19,140 @@ using namespace octopus::hmm::simd;
 BOOST_AUTO_TEST_SUITE(core)
 BOOST_AUTO_TEST_SUITE(model)
 
-auto add_pad(std::string seq, const int pad = 8)
-{
-    return std::string(pad, 'N') + std::move(seq) + std::string(pad, 'N');
-}
-
-auto add_pad(std::vector<std::int8_t> quals, const int pad = 8, std::int8_t val = 50)
-{
-    quals.resize(quals.size() + 2 * pad, 50);
-    std::rotate(quals.begin(), quals.begin() + pad, quals.end());
-    return quals;
-}
-
-auto align_helper(const std::string& target,
-                  const std::string& query,
-                  const std::vector<std::int8_t>& qualities,
-                  int gap_open, int gap_extend, int nuc_prior,
-                  int pad = 8)
-{
-    const auto padded_target = add_pad(target, pad);
-    const auto padded_qualities = add_pad(qualities, pad);
-    return align(padded_target.data(), query.data(), padded_qualities.data(),
-                 static_cast<int>(padded_target.size()), static_cast<int>(query.size()),
-                 gap_open, gap_extend, nuc_prior);
-}
-
-BOOST_AUTO_TEST_CASE(correct_alignment_score)
+struct TestCase
 {
     std::string target, query;
-    std::vector<std::int8_t> qualities;
+    std::vector<std::int8_t> base_qualities;
+    std::vector<std::int8_t> gap_open;
+    int gap_extend, nuc_prior;
+    unsigned band_size = 8;
+};
+
+struct Alignment
+{
     int score;
+    int begin;
+    std::string target, query;
+};
+
+std::ostream& operator<<(std::ostream& os, const std::vector<std::int8_t>& quals)
+{
+    std::copy(std::cbegin(quals), std::cend(quals), std::ostream_iterator<std::int8_t> {os, " "});
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const TestCase& test)
+{
+    os << "target: " << test.target << '\n';
+    os << "query: " << test.query << '\n';
+    os << "base_qualities: " << test.base_qualities << '\n';
+    os << "gap_open: " << test.gap_open << '\n';
+    os << "gap_extend: " << test.gap_extend << '\n';
+    os << "nuc_prior: " << test.nuc_prior << '\n';
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const Alignment& alignment)
+{
+    os << "score: " << alignment.score << '\n';
+    os << "begin: " << alignment.begin << '\n';
+    os << "target: " << alignment.target << '\n';
+    os << "query: " << alignment.query << '\n';
+    return os;
+}
+
+Alignment
+align_helper(TestCase test)
+{
+    Alignment result {};
+    std::vector<char> align1(2 * test.target.size() + 1, '\0'), align2(2 * test.target.size() + 1, '\0');
+    result.score = align(test.target.data(), test.query.data(), test.base_qualities.data(),
+                         static_cast<int>(test.target.size()), static_cast<int>(test.query.size()),
+                         test.gap_open.data(), test.gap_extend, test.nuc_prior,
+                         result.begin, align1.data(), align2.data());
+    result.target.assign(align1.cbegin(), std::find(align1.cbegin(), align1.cend(), '\0'));
+    result.query.assign(align2.cbegin(), std::find(align2.cbegin(), align2.cend(), '\0'));
+    return result;
+}
+
+#define CHECK_ALIGNMENT(received, expected) \
+    BOOST_CHECK_EQUAL(received.score, expected.score); \
+    BOOST_CHECK_EQUAL(received.begin, expected.begin); \
+    BOOST_CHECK_EQUAL(received.target, expected.target); \
+    BOOST_CHECK_EQUAL(received.query, expected.query);
+
+BOOST_AUTO_TEST_CASE(check_alignments)
+{
+    TestCase test;
+    Alignment expected_alignment;
     
-    target = "AAAAAAAAAAAAAAAAAA";
-    query = "AAAAAAAAAAAAAAAAAA";
-    qualities.assign(query.size(), 30);
-    score = align_helper(target, query, qualities, 45, 3, 50);
-    BOOST_CHECK_EQUAL(score, 0);
+    // test 1
+    test = {
+        "ACGTACGTACGTACGAAAA",
+        "AAAA",
+        {40,40,40,40},
+        {10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10},
+        1,
+        4
+    };
+    expected_alignment = {
+        0,
+        15,
+        "AAAA",
+        "AAAA"
+    };
+    CHECK_ALIGNMENT(align_helper(test), expected_alignment);
+
+    // test 2
+    test = {
+        "ACGTACGTACGTACGAATA",
+        "AAAA",
+        {40,40,40,40},
+        {90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90},
+        1,
+        4
+    };
+    expected_alignment = {
+        40,
+        15,
+        "AATA",
+        "AAAA"
+    };
+    CHECK_ALIGNMENT(align_helper(test), expected_alignment);
     
-    target = "AAAAAAACCCCAAAAAAA";
-    query = "AAAAAAACCCCAAAAAAA";
-    qualities.assign(query.size(), 30);
-    score = align_helper(target, query, qualities, 45, 3, 50);
-    BOOST_CHECK_EQUAL(score, 0);
-    
-    target = "AAAAAAACCCCAAAAAAA";
-    query = "AAAAAAACACCAAAAAAA";
-    qualities.assign(query.size(), 30);
-    score = align_helper(target, query, qualities, 45, 3, 50);
-    BOOST_CHECK_EQUAL(score, 30);
+    // test 3
+    test = {
+        "ACGTACGTACGTACGAAGC",
+        "CGGC",
+        {40,40,40,40},
+        {90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,70,90,90,90},
+        1,
+        4
+    };
+    expected_alignment = {
+        71,
+        13,
+        "CGAAGC",
+        "CG--GC"
+    };
+    CHECK_ALIGNMENT(align_helper(test), expected_alignment);
+
+    // test 4
+    test = {
+        "CGAAGCACGTACGTACGTA",
+        "CGGC",
+        {40,40,40,40},
+        {90,90,70,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90},
+        1,
+        4
+    };
+    expected_alignment = {
+        71,
+        0,
+        "CGAAGC",
+        "CG--GC"
+    };
+    CHECK_ALIGNMENT(align_helper(test), expected_alignment);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
