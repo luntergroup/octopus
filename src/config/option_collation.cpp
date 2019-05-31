b4f3211c294609361a82cef077bcbdb2652fa514
Daniel Cooke <dcooke@well.ox.ac.uk>
2019-05-31 11:51:36 +0100

    Add structual variant trigger to AssemblerActiveRegionGenerator

--- src/config/option_collation.cpp
+++ src/config/option_collation.cpp
@@ -1128,6 +1128,9 @@ auto make_variant_generator_builder(const OptionMap& options)
         active_region_options.assemble_all = true;
     } else {
         AssemblerActiveRegionGenerator::Options assembler_region_options {};
+        using TT = AssemblerActiveRegionGenerator::Options::TriggerType;
+        assembler_region_options.trigger_types = {TT::indel, TT::structual};
+        assembler_region_options.read_profile = read_profile;
         assembler_region_options.min_expected_mutation_frequency = get_assembler_region_generator_frequency_trigger(options);
         active_region_options.assembler_active_region_generator_options = assembler_region_options;
     }
