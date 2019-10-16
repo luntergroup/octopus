e5f30693bbd3285728658a42cc2f68b7805a5b91
Daniel <dcooke@well.ox.ac.uk>
2019-10-16 14:21:07 +0000

    Reduce low depth threshold for bad region depth prob

--- src/core/tools/bad_region_detector.cpp
+++ src/core/tools/bad_region_detector.cpp
@@ -437,7 +437,7 @@ auto discrete_sf(const Range& probabilities, std::size_t x)
 
 auto calculate_conditional_depth_probability(const unsigned target_depth, const ReadSetProfile::DepthStats& stats)
 {
-    const auto low_depth = stats.positive.mean + std::min(stats.positive.median, stats.positive.stdev);
+    const auto low_depth = stats.positive.mean + std::min(stats.positive.median, stats.positive.stdev) / 2;
     double result {1};
     if (low_depth < target_depth) {
         result *= discrete_sf(stats.distribution, target_depth);
