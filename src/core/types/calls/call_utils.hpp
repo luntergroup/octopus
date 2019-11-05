c21b61498a29eb63bb215e90858a545811ad5af7
Daniel Cooke <dcooke@well.ox.ac.uk>
2019-11-05 14:20:57 +0000

    Make GT reordering phase set aware

--- src/core/types/calls/call_utils.hpp
+++ src/core/types/calls/call_utils.hpp
@@ -33,10 +33,41 @@ void sort_genotype_alleles(Iterator first_call, Iterator last_call, const Sample
     }
 }
 
+namespace detail {
+
+bool are_in_phase(const Call::GenotypeCall& lhs, const Call::GenotypeCall& rhs)
+{
+    return lhs.phase && rhs.phase && overlaps(lhs.phase->region(), rhs.phase->region());
+}
+
+bool are_in_phase(const CallWrapper& lhs, const CallWrapper& rhs, const SampleName& sample)
+{
+    return are_in_phase(lhs->get_genotype_call(sample), rhs->get_genotype_call(sample));
+}
+
+} // namespacedetail
+
+template <typename Iterator>
+void sort_genotype_alleles_by_phase_set(Iterator first_call, Iterator last_call, const SampleName& sample)
+{
+    while (first_call != last_call) {
+        const auto phased = [&] (const CallWrapper& call) { return detail::are_in_phase(call, *first_call, sample); };
+        const auto last_phased_call = std::find_if_not(std::next(first_call), last_call, phased);
+        sort_genotype_alleles(first_call, last_phased_call, sample);
+        first_call = last_phased_call;
+    }
+}
+
 template <typename Range>
 void sort_genotype_alleles(Range& calls, const SampleName& sample)
 {
     sort_genotype_alleles(std::begin(calls), std::end(calls), sample);
+}
+
+template <typename Range>
+void sort_genotype_alleles_by_phase_set(Range& calls, const SampleName& sample)
+{
+    sort_genotype_alleles_by_phase_set(std::begin(calls), std::end(calls), sample);
 }
 
 template <typename Iterator>
@@ -47,10 +78,24 @@ void sort_genotype_alleles(Iterator first_call, Iterator last_call, const std::v
     }
 }
 
+template <typename Iterator>
+void sort_genotype_alleles_by_phase_set(Iterator first_call, Iterator last_call, const std::vector<SampleName>& samples)
+{
+    for (const auto& sample : samples) {
+        sort_genotype_alleles_by_phase_set(first_call, last_call, sample);
+    }
+}
+
 template <typename Range>
 void sort_genotype_alleles(Range& calls, const std::vector<SampleName>& samples)
 {
     sort_genotype_alleles(std::begin(calls), std::end(calls), samples);
+}
+
+template <typename Range>
+void sort_genotype_alleles_by_phase_set(Range& calls, const std::vector<SampleName>& samples)
+{
+    sort_genotype_alleles_by_phase_set(std::begin(calls), std::end(calls), samples);
 }
 
 } // namespace octopus
