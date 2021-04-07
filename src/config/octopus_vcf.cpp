a97cba89b85c975efa5f2e0c0cea4cc0c2f29560
Daniel Cooke <dcooke@well.ox.ac.uk>
2021-04-07 20:31:22 +0100

    Make Phase set (PS) VCF field an Integer

--- src/config/octopus_vcf.cpp
+++ src/config/octopus_vcf.cpp
@@ -26,7 +26,7 @@ VcfHeader::Builder make_header_template()
     result.add_format("DP", "1", "Integer", "Read depth at this position for this sample");
     result.add_format("FT", "1", "String", "Sample genotype filter indicating if this genotype was 'called'");
     result.add_format("GQ", "1", "Integer", "Conditional genotype quality (phred-scaled)");
-    result.add_format("PS", "1", "String", "Phase set");
+    result.add_format("PS", "1", "Integer", "Phase set");
     result.add_format("PQ", "1", "Integer", "Phasing quality");
     result.add_format("MQ", "1", "Integer", "RMS mapping quality");
     
