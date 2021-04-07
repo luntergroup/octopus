7ce50979b888774a35d1d7e4dac1b0a36b0aca6c
Daniel Cooke <dcooke@well.ox.ac.uk>
2021-04-07 10:30:06 +0100

    Update copyright year to 2021

--- src/core/callers/caller_factory.cpp
+++ src/core/callers/caller_factory.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2015-2020 Daniel Cooke
+// Copyright (c) 2015-2021 Daniel Cooke
 // Use of this source code is governed by the MIT license that can be found in the LICENSE file.
 
 #include "caller_factory.hpp"
